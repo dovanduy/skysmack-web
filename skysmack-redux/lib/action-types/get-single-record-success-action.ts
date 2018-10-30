921a41473caf3ef49ad3e29c545c7c31db0db053
Martin Jul Hammer <martin@itinstituttet.dk>
2018-10-30 11:41:21 +0100

    Changed Record to lowercase

--- skysmack-redux/lib/action-types/get-single-record-success-action.ts
+++ skysmack-redux/lib/action-types/get-single-record-success-action.ts
@@ -1,8 +1,9 @@
 import { GetSingleRecordAction } from './get-single-record-action';
 import { Record } from 'skysmack-framework';
 
-export class GetSingleRecordSuccessAction<TRecord extends Record<TKey>, TKey> extends GetSingleRecordAction<TKey> {  
-  public Record: TRecord;
+export class GetSingleRecordSuccessAction<TRecord extends Record<TKey>, TKey> extends GetSingleRecordAction<TKey> {
+  public record: TRecord;
+  public test: boolean;
 
   public constructor(init?: Partial<GetSingleRecordSuccessAction<TRecord, TKey>>) {
     super(init);
--- skysmack-redux/lib/reducers/record-reducers-base.ts
+++ skysmack-redux/lib/reducers/record-reducers-base.ts
@@ -3,7 +3,7 @@ import { GetPagedRecordsSuccessAction, GetSingleRecordSuccessAction, PackageActi
 import { Record, LocalObject, RecordExtensions, PageExtensions } from "skysmack-framework";
 import { RecordState } from "../states";
 
-export function recordReducersBase<TRecord extends Record<TKey>, TKey>(state: RecordState<TRecord, TKey>, action: PackageAction, prefix: string = ''): RecordState<TRecord, TKey> {   
+export function recordReducersBase<TRecord extends Record<TKey>, TKey>(state: RecordState<TRecord, TKey>, action: PackageAction, prefix: string = ''): RecordState<TRecord, TKey> {
     switch (action.type) {
         case prefix + RecordActionsBase.GET_PAGED_SUCCESS:
             const getPagedEntitiesSuccessAction = action as GetPagedRecordsSuccessAction<TRecord, TKey>;
@@ -14,8 +14,8 @@ export function recordReducersBase<TRecord extends Record<TKey>, TKey>(state: Re
 
         case prefix + RecordActionsBase.GET_SINGLE_SUCCESS:
             const getSingleEntitySuccessAction = action as GetSingleRecordSuccessAction<TRecord, TKey>;
-            var localObject = new LocalObject<TRecord>({ object: getSingleEntitySuccessAction.Record });
-            state.localRecords[getSingleEntitySuccessAction.packagePath] = RecordExtensions.mergeOrAddLocalRecords(state.localRecords[getSingleEntitySuccessAction.packagePath], [ localObject ]);
+            var localObject = new LocalObject<TRecord>({ object: getSingleEntitySuccessAction.record });
+            state.localRecords[getSingleEntitySuccessAction.packagePath] = RecordExtensions.mergeOrAddLocalRecords(state.localRecords[getSingleEntitySuccessAction.packagePath], [localObject]);
             break;
 
         default:
