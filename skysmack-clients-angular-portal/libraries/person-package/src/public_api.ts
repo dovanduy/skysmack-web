282d22c268bcdc773affadaef1b51fa4566b220b
Martin Jul Hammer <martin@itinstituttet.dk>
2018-10-31 10:25:31 +0100

    Removed ng-packgr persons package

--- skysmack-clients-angular-portal/libraries/person-package/src/public_api.ts
+++
@@ -1,7 +0,0 @@
-/*
- * Public API Surface of person-package
- */
-
-export * from './lib/persons/redux/persons-requests';
-export * from './lib/person-package.service';
-export * from './lib/person-package.module';
