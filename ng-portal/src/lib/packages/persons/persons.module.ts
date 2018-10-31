122fe7897d60bf747e17ee91c28a3d91887cab6a
Martin Jul Hammer <martin@itinstituttet.dk>
2018-10-31 15:16:48 +0100

    Added persons module

---
+++ ng-portal/src/lib/packages/persons/persons.module.ts
@@ -0,0 +1,13 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { PersonsRoutingModule } from './persons-routing.module';
+
+@NgModule({
+  imports: [
+    CommonModule,
+    PersonsRoutingModule
+  ],
+  declarations: []
+})
+export class PersonsModule { }
