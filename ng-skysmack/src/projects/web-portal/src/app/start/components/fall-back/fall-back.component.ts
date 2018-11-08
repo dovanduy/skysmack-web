f202c6a5017e98bde89594eb930a61c60dac86aa
Martin Jul Hammer <martin@itinstituttet.dk>
2018-11-08 10:15:08 +0100

    Added fallback code

--- ng-skysmack/src/projects/web-portal/src/app/start/components/fall-back/fall-back.component.ts
+++ ng-skysmack/src/projects/web-portal/src/app/start/components/fall-back/fall-back.component.ts
@@ -1,4 +1,11 @@
 import { Component, OnInit } from '@angular/core';
+import { Observable } from 'rxjs';
+import { CurrentTenantViewModel } from '@skysmack/packages-skysmack';
+import { Router, ActivatedRoute } from '@angular/router';
+import { PackageRouteConfiguration } from '../../../package-route-configuration';
+import { NgSkysmackRedux } from 'lib/ng-packages/skysmack/redux/ng-skysmack-redux';
+
+// export class FallBackComponent extends EntityBase implements OnInit {
 
 @Component({
   selector: 'ss-fall-back',
@@ -7,9 +14,56 @@ import { Component, OnInit } from '@angular/core';
 })
 export class FallBackComponent implements OnInit {
 
-  constructor() { }
+  public currentTenant$: Observable<CurrentTenantViewModel>;
+  public packagesLoaded = false;
 
-  ngOnInit() {
+
+  constructor(
+    // public componentPageTitle: EntityComponentPageTitle,
+    public router: Router,
+    public activatedRoute: ActivatedRoute,
+    public redux: NgSkysmackRedux,
+    public packageRouteConfiguration: PackageRouteConfiguration,
+  ) {
+    // super(router, activatedRoute, redux);
   }
 
+  ngOnInit() {
+    this.currentTenant$ = this.redux.getCurrentTenant();
+    // this.componentPageTitle.setTitle('Not found');
+    // super.ngOnInit();
+    this.checkForTenantLoaded();
+  }
+
+  private checkForTenantLoaded() {
+    // TODO: Uncomment this when entity base is implemented
+    // this.subscriptionHandler.subscribe(this.packageRouteConfiguration.configure());
+    // this.subscriptionHandler.subscribe(this.currentTenant$.subscribe((tenant) => {
+    //   if (tenant != null && tenant.name.length > 0) {
+    //     if (this.router.onSameUrlNavigation !== 'reload') {
+    //       this.router.onSameUrlNavigation = 'reload';
+    //       this.router.navigate([this.router.url]);
+    //     } else {
+    //       this.router.onSameUrlNavigation = 'ignore';
+    //     }
+    //     this.componentPageTitle.setTitle('Not found');
+    //     this.packagesLoaded = true;
+    //   }
+    // }));
+
+    // TODO: Remove this when entity base is implemented
+    this.packageRouteConfiguration.configure();
+    this.currentTenant$.subscribe((tenant) => {
+      if (tenant != null && tenant.name.length > 0) {
+        if (this.router.onSameUrlNavigation !== 'reload') {
+          this.router.onSameUrlNavigation = 'reload';
+          // this.router.navigate([this.router.url]);
+        } else {
+          this.router.onSameUrlNavigation = 'ignore';
+        }
+        // this.componentPageTitle.setTitle('Not found');
+        this.packagesLoaded = true;
+      }
+    });
+  }
 }
