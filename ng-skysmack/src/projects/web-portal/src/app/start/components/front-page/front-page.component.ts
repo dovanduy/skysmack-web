5ff6f10092e63f735ad2239d39a030803a9abcce
Martin Jul Hammer <martin@itinstituttet.dk>
2018-11-09 15:34:57 +0100

    Added skysmack title

--- ng-skysmack/src/projects/web-portal/src/app/start/components/front-page/front-page.component.ts
+++ ng-skysmack/src/projects/web-portal/src/app/start/components/front-page/front-page.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { EntityComponentPageTitle } from 'lib/portal-ui/models/entity-component-page-title';
 
 @Component({
   selector: 'ss-front-page',
@@ -7,9 +8,10 @@ import { Component, OnInit } from '@angular/core';
 })
 export class FrontPageComponent implements OnInit {
 
-  constructor() { }
+  constructor(public componentPageTitle: EntityComponentPageTitle) { }
 
   ngOnInit() {
+    this.componentPageTitle.setTitle('Skysmack', true);
   }
 
 }
