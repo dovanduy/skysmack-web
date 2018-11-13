8ebc6546d2a625cf62ee68a53ca422612d411eea
Martin Jul Hammer <martin@itinstituttet.dk>
2018-11-13 09:26:41 +0100

    Added persosn module to start module

--- ng-skysmack/src/projects/web-portal/src/app/start/start.module.ts
+++ ng-skysmack/src/projects/web-portal/src/app/start/start.module.ts
@@ -1,3 +1,6 @@
+// Do not deletes
+import { PersonsModule } from 'lib/portal-packages/persons/persons.module';
+
 import { NgReduxRouter, NgReduxRouterModule } from '@angular-redux/router';
 import { TranslateModule, TranslateLoader } from '@ngx-translate/core';
 import { HttpClientModule, HttpClient } from '@angular/common/http';
@@ -15,6 +18,7 @@ import { FallBackComponent } from './components/fall-back/fall-back.component';
 import { StartComponent } from './components/start/start.component';
 import { configureRedux } from '../redux/redux.configuration';
 import { applicationStartup } from './application-startup';
+
 
 // AoT requires an exported function for factories
 export function HttpLoaderFactory(http: HttpClient) {
