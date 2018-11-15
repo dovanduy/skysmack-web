42474fae31a5b7e9cd9ef713f1b4b0420365838f
Martin Jul Hammer <martin@itinstituttet.dk>
2018-11-15 11:03:04 +0100

    Refactored registerWithRootEpic

--- redux/lib/epics/root-epic.ts
+++ redux/lib/epics/root-epic.ts
@@ -4,9 +4,7 @@ import { combineEpics, Epic } from 'redux-observable';
 
 export const epic$ = new BehaviorSubject(combineEpics());
 
-export function registerWithRootEpic(epics: Epic[]) {
-    epics.forEach(epic => epic$.next(epic));
-}
+export const registerWithRootEpic = (epics: Epic[]) => epics.forEach(epic => epic$.next(epic))
 
 export const rootEpic = (action$, state$) => epic$.pipe(
     mergeMap(epic => epic(action$, state$, undefined))
