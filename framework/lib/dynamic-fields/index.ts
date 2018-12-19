f7616b9b1c999eccfdbcf646e7b8c325bf63bbb7
Martin Jul Hammer <mjh@itinstituttet.dk>
2018-12-19 13:03:03 +0100

    Moved all field related files to own folder in framework.

---
+++ framework/lib/dynamic-fields/index.ts
@@ -0,0 +1,4 @@
+export * from './field-schema-view-model';
+export * from './field-value-provider-view-model';
+export * from './field-validatior';
+export * from './field-access-permission';
--- framework/lib/index.ts
+++ framework/lib/index.ts
@@ -2,3 +2,4 @@ export * from './helpers';
 export * from './models';
 export * from './rsql';
 export * from './sort';
+export * from './dynamic-fields';
