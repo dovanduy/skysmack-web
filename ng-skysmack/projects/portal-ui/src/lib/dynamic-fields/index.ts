1fb89e9e806f6ffe85592229a1d410a3460c2a53
Morten Aslo-Petersen <morten@itinstituttet.dk>
2019-01-02 15:49:38 +0100

    Missing files in index.ts

--- ng-skysmack/projects/portal-ui/src/lib/dynamic-fields/index.ts
+++ ng-skysmack/projects/portal-ui/src/lib/dynamic-fields/index.ts
@@ -1,3 +1,6 @@
 export * from './dynamic-fields-routing.module';
 export * from './dynamic-fields.module';
 export * from './components';
+export * from './ng-dynamic-fields-config';
+export * from './ng-dynamic-fields-menu';
+export * from './ng-dynamic-fields-validation';
