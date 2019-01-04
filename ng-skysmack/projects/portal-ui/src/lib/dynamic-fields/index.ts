9dcbe208fa2ae62ccc2cf447b6a105a45b234c98
Morten Aslo-Petersen <morten@itinstituttet.dk>
2019-01-04 16:19:50 +0100

    Build works

--- ng-skysmack/projects/portal-ui/src/lib/dynamic-fields/index.ts
+++ ng-skysmack/projects/portal-ui/src/lib/dynamic-fields/index.ts
@@ -1,5 +1,4 @@
 export * from './components/index';
-export * from './dynamic-fields-routing.module';
 export * from './dynamic-fields.module';
 export * from './ng-dynamic-fields-config';
 export * from './ng-dynamic-fields-menu';
