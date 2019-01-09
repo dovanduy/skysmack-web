bc00606477b63202b8e39cde0bcca23ca4a274b8
Martin Jul Hammer <mjh@itinstituttet.dk>
2019-01-09 14:46:55 +0100

    Added build icon for maintenance packages.

--- ng-skysmack/src/web-portal/src/app/packages/maintenance-package-manifest.ts
+++ ng-skysmack/src/web-portal/src/app/packages/maintenance-package-manifest.ts
@@ -3,7 +3,7 @@ import { PackageManifest } from '@skysmack/ng-ui';
 import { PackageLoader } from '@skysmack/ng-packages';
 
 export class MaintenancePackageManifest extends MaintenanceType implements PackageManifest {
-    public icon = 'face';
+    public icon = 'build';
     public menuLocation = 'main';
     public modulePath = './../packages/modules/maintenance_wrapper.module#MaintenanceWrapperModule';
 }
