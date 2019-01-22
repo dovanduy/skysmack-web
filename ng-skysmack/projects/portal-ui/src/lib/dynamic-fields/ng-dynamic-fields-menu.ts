e181023f90b4513d9a7a8181903a4945678f48cf
Martin Jul Hammer <mjh@itinstituttet.dk>
2019-01-22 15:42:00 +0100

    Fixed back button for dynamic fields.

--- ng-skysmack/projects/portal-ui/src/lib/dynamic-fields/ng-dynamic-fields-menu.ts
+++ ng-skysmack/projects/portal-ui/src/lib/dynamic-fields/ng-dynamic-fields-menu.ts
@@ -1,5 +1,5 @@
 import { Injectable, Inject } from '@angular/core';
-import { Router } from '@angular/router';
+import { Router, ActivatedRoute, ActivationEnd } from '@angular/router';
 import { SidebarMenu } from './../models/sidebar-menu/sidebar-menu';
 import { NgSkysmackStore } from '@skysmack/ng-packages';
 import { MenuItemProvider } from '@skysmack/ng-ui';
@@ -14,7 +14,7 @@ export class NgDynamicFieldsMenu extends SidebarMenu {
     constructor(
         public redux: NgSkysmackStore,
         public router: Router,
-        @Inject(MenuItemProvider.TOKEN) menuItemProviders: MenuItemProvider[],
+        @Inject(MenuItemProvider.TOKEN) menuItemProviders: MenuItemProvider[]
     ) {
         super(redux, router, menuItemProviders);
         this.setPrimaryMenu();
@@ -26,6 +26,22 @@ export class NgDynamicFieldsMenu extends SidebarMenu {
         this.primaryMenuAreas.push(new MenuArea('actions', this.translationPrefix, 1));
         this.primaryMenuAreas.push(new MenuArea('manage', this.translationPrefix, 2));
 
+        // Since the below router subscription isn't fired on first page load, initial menu items have to be set.
+        this.addPrimaryMenuItems();
+
+        // Reset the menu items for dynamic fields on navigation to a fields page.
+        // Otherwise these will only be set once when first navigating to a fields page
+        // (since this class is only instantiated once because dependency injection).
+        this.router.events.subscribe((event) => {
+            if (event instanceof ActivationEnd && event.snapshot.url.find(urlSegment => urlSegment.path === 'fields')) {
+                this.setPaths();
+                this.primaryMenuItems = [];
+                this.addPrimaryMenuItems();
+            }
+        });
+    }
+
+    public addPrimaryMenuItems() {
         this.primaryMenuItems.push(new MenuItem('create', this.translationPrefix + 'CREATE', 'actions', 1, 'groupAdd'));
         this.primaryMenuItems.push(new MenuItem(`/${this.packagePath}/${this.additionalPaths.join('/')}`, this.translationPrefix + 'BACK', 'manage', 2, 'arrowBack'));
     }
