00214924168a56cba41712bde4d2a433d341c5a4
Martin Jul Hammer <mjh@itinstituttet.dk>
2019-01-23 15:56:18 +0100

    Hooked up access policy epics and reducers.

--- ng-skysmack/projects/ng-packages/src/lib/skysmack-core/access-policies/ng-access-policies.module.ts
+++ ng-skysmack/projects/ng-packages/src/lib/skysmack-core/access-policies/ng-access-policies.module.ts
@@ -1,20 +1,27 @@
 import { NgModule } from '@angular/core';
+import { NgAccessPolicyPermissionsEpics } from './redux/access-policy-permissions/ng-access-policy-permissions-epics';
+import { NgAccessPolicyRolesEpics } from './redux/access-policy-roles/ng-access-policy-roles-epics';
+import { NgAccessPolicyRulesEpics } from './redux/access-policy-rules/ng-access-policy-rules-epics';
+import { ReducerRegistry } from '@skysmack/redux';
+import { accessPolicyPermissionsReducer, accessPolicyRolesReducer, accessPolicyRulesReducer } from '@skysmack/packages-skysmack-core';
+import { registerEpics } from '@skysmack/ng-redux';
 
 @NgModule({
   imports: [],
   exports: [],
-  providers: [
-    [
-      // { provide: 'NgPersonsActions', useClass: NgPersonsActions },
-      // { provide: 'NgPersonsStore', useClass: NgPersonsStore }
-    ]
-  ],
+  providers: []
 })
 export class NgAccessPoliciesModule {
   constructor(
-    // epics: NgPersonsEpics
+    permissionsEpics: NgAccessPolicyPermissionsEpics,
+    rolesEpics: NgAccessPolicyRolesEpics,
+    rulesEpics: NgAccessPolicyRulesEpics
   ) {
-    // ReducerRegistry.Instance.register('persons', personsReducer);
-    // registerEpics(epics);
+    ReducerRegistry.Instance.register('accessPolicyPermissions', accessPolicyPermissionsReducer);
+    ReducerRegistry.Instance.register('accessPolicyRoles', accessPolicyRolesReducer);
+    ReducerRegistry.Instance.register('accessPolicyRules', accessPolicyRulesReducer);
+    registerEpics(permissionsEpics);
+    registerEpics(rolesEpics);
+    registerEpics(rulesEpics);
   }
 }
