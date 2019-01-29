a4aad8ca0abb65045be40e9002da62532c2712a0
Martin Jul Hammer <mjh@itinstituttet.dk>
2019-01-29 14:29:35 +0100

    Cleaned person notifications

--- ng-skysmack/projects/ng-packages/src/lib/persons/ng-persons-notifications.ts
+++ ng-skysmack/projects/ng-packages/src/lib/persons/ng-persons-notifications.ts
@@ -1,24 +1,8 @@
 import { Injectable, Inject } from '@angular/core';
 import { Person } from '@skysmack/packages-persons';
 import { DocumentRecordNotifications, Notifications } from '@skysmack/ng-redux';
-import { ReduxAction, CommitMeta } from '@skysmack/redux';
-import { LocalObject } from '@skysmack/framework';
 
 @Injectable({ providedIn: 'root' })
 export class NgPersonsNotifications extends DocumentRecordNotifications<Person, number> {
-
-    protected defaultTranslationString = 'PERSONS.NOTIFICATIONS.';
-
-    constructor(
-        @Inject('Notifications') public notifications: Notifications
-    ) {
-        super(notifications);
-    }
-
-    // // Custom notification example.
-    // public addSuccess(action: ReduxAction<unknown, CommitMeta<LocalObject<Person, number>[]>>) {
-    //     const translationString = 'PERSONS.NOTIFICATIONS.ADD.SUCCESS';
-    //     const translationParams = { 0: action.meta.value[0].object.displayName };
-    //     this.notifications.showTranslatedSnackbarMessage(translationString, translationParams, undefined, 2000);
-    // }
+    constructor(@Inject('Notifications') public notifications: Notifications) { super(notifications); }
 }
