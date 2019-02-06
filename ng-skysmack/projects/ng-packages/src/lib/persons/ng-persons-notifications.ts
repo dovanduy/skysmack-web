fd02baca09633a285fa3f71e7c6af9737b63667c
Martin Jul Hammer <mjh@itinstituttet.dk>
2019-02-06 10:33:21 +0100

    Fixed bug for recordformcomponent

--- ng-skysmack/projects/ng-packages/src/lib/persons/ng-persons-notifications.ts
+++ ng-skysmack/projects/ng-packages/src/lib/persons/ng-persons-notifications.ts
@@ -1,8 +1,14 @@
 import { Injectable, Inject } from '@angular/core';
 import { Person } from '@skysmack/packages-persons';
 import { DocumentRecordNotifications, Notifications } from '@skysmack/ng-redux';
+import { ReduxAction, CommitMeta } from '@skysmack/redux';
+import { LocalObject } from '@skysmack/framework';
 
 @Injectable({ providedIn: 'root' })
 export class NgPersonsNotifications extends DocumentRecordNotifications<Person, number> {
     constructor(@Inject('Notifications') public notifications: Notifications) { super(notifications); }
+
+    public addSuccess(action: ReduxAction<unknown, CommitMeta<LocalObject<Person, number>[]>>) {
+        this.notifications.showTranslatedSnackbarMessage(this.defaultTranslationString + 'ADD.SUCCESS', { 0: action.meta.value[0].object.displayName }, undefined, 2000);
+    }
 }
