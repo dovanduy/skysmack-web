a6896600304063709d8d613d92167e78225bd054
Martin Jul Hammer <mjh@itinstituttet.dk>
2019-02-08 14:37:51 +0100

    Bound initial props for records container.

--- ng-skysmack/projects/portal-ui/src/lib/components/common/records-container/records-container.component.ts
+++ ng-skysmack/projects/portal-ui/src/lib/components/common/records-container/records-container.component.ts
@@ -1,7 +1,6 @@
 import { Component, OnInit, Input, EventEmitter, Output } from '@angular/core';
 import { LocalObject, LoadingState, SubscriptionHandler } from '@skysmack/framework';
 import { Observable } from 'rxjs';
-import { MenuItem } from '@skysmack/ng-ui/lib/models/sidebar-menu/menu-item';
 import { EntityAction } from '@skysmack/ng-ui';
 
 @Component({
@@ -10,7 +9,6 @@ import { EntityAction } from '@skysmack/ng-ui';
   styleUrls: ['./records-container.component.scss']
 })
 export class RecordsContainerComponent implements OnInit {
-
   // Input properties defined in the task
   @Input() public entities$: Observable<LocalObject<any, any>[]>;
   @Input() public totalCount: number;
@@ -19,24 +17,21 @@ export class RecordsContainerComponent implements OnInit {
 
   // Output properties defined in the task
   @Output() public loadNext = new EventEmitter<boolean>(); // Is the emit type correct?
-  @Output() public sort = new EventEmitter<string>(); // Is the emit type correct? Could be PagedQuery
-  @Output() public filter = new EventEmitter<string>(); // Is the emit type correct? Could be PagedQuery
+  // @Output() public sort = new EventEmitter<any>();
+  // @Output() public filter = new EventEmitter<any>();
 
   // Properties taken from Data table
   @Input() public packagePath: string;
   @Input() public title: string;
   @Input() public cancelAction: Function;
   @Input() public displayedColumns: string[] = [];
-  @Output() public delete = new EventEmitter<LocalObject<any, any>>();
-  public subscriptionHandler = new SubscriptionHandler();
+  // public subscriptionHandler = new SubscriptionHandler();
 
   constructor() { }
 
   ngOnInit() {
-  }
-
-  public deleteEntity(entity: LocalObject<any, any>) {
-    this.delete.emit(entity);
+    console.log('entities: ' + this.entities$ + '\n\n', 'totalCount: ' + this.totalCount + '\n\n', 'loadingState: ' + this.loadingState + '\n\n', 'entityActions: ' + this.entityActions + '\n\n', 'loadNext: ' + this.loadNext + '\n\n');
+    console.log('packagePath: ' + this.packagePath + '\n\n', 'title: ' + this.title + '\n\n', 'cancelAction: ' + this.cancelAction + '\n\n', 'displayedColumns: ' + this.displayedColumns + '\n\n');
   }
 
   public runCancelAction(entity: LocalObject<any, any>) {
