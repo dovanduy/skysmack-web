24e1cba457c30b027ad42bd514902d41b3052dad
Christian Vilen <christian-vilen@itinstituttet.dk>
2019-02-13 10:05:51 +0100

    snackbar for packages

--- ng-skysmack/projects/ng-packages/src/lib/skysmack-core/packages/redux/ng-packages-epics.ts
+++ ng-skysmack/projects/ng-packages/src/lib/skysmack-core/packages/redux/ng-packages-epics.ts
@@ -1,22 +1,31 @@
 import { ofType, ActionsObservable, Epic } from 'redux-observable';
-import { ReduxAction, PackagePathPayload, GetAvailablePackagesSuccessPayload } from '@skysmack/redux';
-import { switchMap } from 'rxjs/operators';
+import { ReduxAction, PackagePathPayload, GetAvailablePackagesSuccessPayload, CommitMeta, RollbackMeta } from '@skysmack/redux';
+import { switchMap, map } from 'rxjs/operators';
 import { Observable } from 'rxjs';
 import { Injectable } from '@angular/core';
 import { NgPackagesRequests } from './ng-packages-requests';
 import { GetPackagesSuccessPayload, GetPackageSuccessPayload } from '@skysmack/packages-skysmack-core';
 import { NgPackagesActions } from './ng-packages-actions';
-import { HttpErrorResponse } from '@skysmack/framework';
+import { HttpErrorResponse, LocalObject, Package } from '@skysmack/framework';
+import { NgPackagesNotifications } from '../ng-packages-notifications';
 
 @Injectable({ providedIn: 'root' })
 export class NgPackagesEpics {
     public epics: Epic[];
 
-    constructor(protected requests: NgPackagesRequests) {
+    constructor(protected requests: NgPackagesRequests, protected notifications: NgPackagesNotifications) {
         this.epics = [
             this.getEpic,
             this.getSingleEpic,
-            this.getAvailablePackagesEpic
+            this.getAvailablePackagesEpic,
+            this.snackBarGetPackageFailureEpic,
+            this.snackBarGetSingleFailureEpic,
+            this.snackBarCreateSuccessEpic,
+            this.snackBarUpdateSuccessEpic,
+            this.snackBarRemoveSuccessEpic,
+            this.snackBarCreateFailureEpic,
+            this.snackBarUpdateFailureEpic,
+            this.snackBarRemoveFailureEpic,
         ];
     }
 
@@ -40,4 +49,68 @@ export class NgPackagesEpics {
             switchMap(() => this.requests.getAvailablePackages())
         );
     }
+
+    public snackBarGetPackageFailureEpic = (action$: ActionsObservable<ReduxAction<HttpErrorResponse, RollbackMeta<LocalObject<Package, string>[]>>>): Observable<ReduxAction> => action$.pipe(
+        ofType(NgPackagesActions.GET_PACKAGES_FAILURE),
+        map((action) => {
+            this.notifications.getPagedError(action);
+            return { type: 'NOTIFICATION' };
+        })
+    )
+
+    public snackBarGetSingleFailureEpic = (action$: ActionsObservable<ReduxAction<HttpErrorResponse, CommitMeta<LocalObject<Package, string>>>>): Observable<ReduxAction> => action$.pipe(
+        ofType(NgPackagesActions.GET_SINGLE_PACKAGE_FAILURE),
+        map((action) => {
+            this.notifications.getSingleError(action);
+            return { type: 'NOTIFICATION' };
+        })
+    )
+
+    public snackBarCreateSuccessEpic = (action$: ActionsObservable<ReduxAction<unknown, CommitMeta<LocalObject<Package, string>[]>>>): Observable<ReduxAction> => action$.pipe(
+        ofType(NgPackagesActions.ADD_PACKAGE_SUCCESS),
+        map((action) => {
+            this.notifications.addSuccess(action);
+            return { type: 'NOTIFICATION' };
+        })
+    )
+
+    public snackBarCreateFailureEpic = (action$: ActionsObservable<ReduxAction<HttpErrorResponse, CommitMeta<LocalObject<Package, string>[]>>>): Observable<ReduxAction> => action$.pipe(
+        ofType(NgPackagesActions.ADD_PACKAGE_FAILURE),
+        map((action) => {
+            this.notifications.addError(action);
+            return { type: 'NOTIFICATION' };
+        })
+    )
+
+    public snackBarUpdateSuccessEpic = (action$: ActionsObservable<ReduxAction<unknown, CommitMeta<LocalObject<Package, string>[]>>>): Observable<ReduxAction> => action$.pipe(
+        ofType(NgPackagesActions.UPDATE_PACKAGE_SUCCESS),
+        map(action => {
+            this.notifications.updateSuccess(action);
+            return { type: 'NOTIFICATION' };
+        })
+    )
+
+    public snackBarUpdateFailureEpic = (action$: ActionsObservable<ReduxAction<HttpErrorResponse, CommitMeta<LocalObject<Package, string>[]>>>): Observable<ReduxAction> => action$.pipe(
+        ofType(NgPackagesActions.UPDATE_PACKAGE_FAILURE),
+        map(action => {
+            this.notifications.updateError(action);
+            return { type: 'NOTIFICATION' };
+        })
+    )
+
+    public snackBarRemoveSuccessEpic = (action$: ActionsObservable<ReduxAction<unknown, CommitMeta<LocalObject<Package, string>[]>>>): Observable<ReduxAction> => action$.pipe(
+        ofType(NgPackagesActions.DELETE_PACKAGE_SUCCESS),
+        map((action) => {
+            this.notifications.removeSuccess(action);
+            return { type: 'NOTIFICATION' };
+        })
+    )
+
+    public snackBarRemoveFailureEpic = (action$: ActionsObservable<ReduxAction<HttpErrorResponse, CommitMeta<LocalObject<Package, string>[]>>>): Observable<ReduxAction> => action$.pipe(
+        ofType(NgPackagesActions.DELETE_PACKAGE_FAILURE),
+        map((action) => {
+            this.notifications.removeError(action);
+            return { type: 'NOTIFICATION' };
+        })
+    )
 }
