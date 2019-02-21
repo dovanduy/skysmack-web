2d1de296660577efb7893a703db0a28c18d5ed80
Martin Jul Hammer <mjh@itinstituttet.dk>
2019-02-21 14:34:40 +0100

    Added comment.

--- ng-skysmack/projects/portal-packages/src/lib/packages/ng-packages-menu.ts
+++ ng-skysmack/projects/portal-packages/src/lib/packages/ng-packages-menu.ts
@@ -26,7 +26,9 @@ export class NgPackagesMenu extends SidebarMenu {
         this.primaryMenuAreas.push(new MenuArea('actions', this.translationPrefix, 1));
         this.primaryMenuAreas.push(new MenuArea('manage', this.translationPrefix, 2));
         this.primaryMenuItems.push(new MenuItem('create', this.translationPrefix + 'CREATE', 'actions', 1, 'groupAdd'));
-        this.primaryMenuItems.push(new MenuItem('available_packages', this.translationPrefix + 'AVAILABLE_PACKAGES', 'manage', 2, 'groupAdd'));
+
+        // Menu button to navigate to available package charts when that code is uncommented.
+        // this.primaryMenuItems.push(new MenuItem('available_packages', this.translationPrefix + 'AVAILABLE_PACKAGES', 'manage', 2, 'groupAdd'));
     }
 
     public setSpeedDialMenu() {
