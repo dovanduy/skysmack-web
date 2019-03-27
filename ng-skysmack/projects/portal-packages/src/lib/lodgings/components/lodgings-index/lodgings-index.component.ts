4f20ca83210791f43a40e193b018fdce400b4968
Christian Vilen <christian-vilen@itinstituttet.dk>
2019-03-27 13:05:31 +0100

    Fixed  'loose' strings for lodgings

--- ng-skysmack/projects/portal-packages/src/lib/lodgings/components/lodgings-index/lodgings-index.component.ts
+++ ng-skysmack/projects/portal-packages/src/lib/lodgings/components/lodgings-index/lodgings-index.component.ts
@@ -2,7 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { EntityComponentPageTitle, DocumentRecordIndexComponent } from '@skysmack/portal-ui';
 import { Router, ActivatedRoute } from '@angular/router';
 import { NgSkysmackStore, NgLodgingsFieldsConfig } from '@skysmack/ng-packages';
-import { LodgingsAppState } from '@skysmack/packages-lodgings';
+import { LodgingsAppState, LODGINGS_AREA_KEY } from '@skysmack/packages-lodgings';
 import { Lodging } from '@skysmack/packages-lodgings';
 import { NgLodgingsActions } from '@skysmack/ng-packages';
 import { NgLodgingsStore } from '@skysmack/ng-packages';
@@ -18,6 +18,7 @@ import { NgFieldActions, NgFieldStore } from '@skysmack/ng-redux';
 })
 export class LodgingsIndexComponent extends DocumentRecordIndexComponent<LodgingsAppState, Lodging, number> implements OnInit {
 
+  public area: string = LODGINGS_AREA_KEY;
   public entityActions: EntityAction[] = [
     new EntityAction().asUrlAction('edit', 'Edit', 'edit'),
     new EntityAction().asEventAction('Delete', this.delete, 'delete', this)
