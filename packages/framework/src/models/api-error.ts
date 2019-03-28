3436c8d6c8c0cd4e5811e50245146b88980e3222
Martin Jul Hammer <mjh@itinstituttet.dk>
2019-03-28 13:02:59 +0100

    Fixed error caused by no validation errors.

--- packages/framework/src/models/api-error.ts
+++ packages/framework/src/models/api-error.ts
@@ -13,19 +13,24 @@ export class ApiError {
         this.detail = response.error.detail;
         this.status = response.error.status;
         this.instance = response.error.instance;
-        this.validationErrors = Object.keys(response.error.validationErrors).map(validationErrorKey => {
-            let fieldNameUpperCase = validationErrorKey.split('.')[1];
-            let fieldKey;
-            if (fieldNameUpperCase) {
-                const firstLetter = fieldNameUpperCase.charAt(0);
-                const lowercaseFirstLetter = fieldNameUpperCase.substr(0, 1).toLowerCase();
-                fieldKey = fieldNameUpperCase.replace(firstLetter, lowercaseFirstLetter);
-            }
 
-            return {
-                fieldKey,
-                errors: response.error.validationErrors[validationErrorKey]
-            } as ValidationError;
-        });
+        if (response.error.validationErrors) {
+            this.validationErrors = Object.keys(response.error.validationErrors).map(validationErrorKey => {
+                let fieldNameUpperCase = validationErrorKey.split('.')[1];
+                let fieldKey;
+                if (fieldNameUpperCase) {
+                    const firstLetter = fieldNameUpperCase.charAt(0);
+                    const lowercaseFirstLetter = fieldNameUpperCase.substr(0, 1).toLowerCase();
+                    fieldKey = fieldNameUpperCase.replace(firstLetter, lowercaseFirstLetter);
+                }
+
+                return {
+                    fieldKey,
+                    errors: response.error.validationErrors[validationErrorKey]
+                } as ValidationError;
+            });
+        } else {
+            this.validationErrors = [];
+        }
     }
 }
