ac4f96fa367d3da0974d9cc2d9976f554b074958
Martin Jul Hammer <mjh@itinstituttet.dk>
2019-04-02 16:06:02 +0200

    "Fixed" circular dependency.

--- ng-skysmack/projects/ng-packages/src/lib/persons-lodging-reservations/ng-persons-lodgings-reservations-field-provider.ts
+++ ng-skysmack/projects/ng-packages/src/lib/persons-lodging-reservations/ng-persons-lodgings-reservations-field-provider.ts
@@ -5,10 +5,11 @@ import { PersonsLodgingReservationsType, PersonsLodgingReservationsSettings } fr
 import { Observable, combineLatest } from 'rxjs';
 import { StrIndex, PagedQuery } from '@skysmack/framework';
 import { NgSettingsActions, NgSettingsStore } from '@skysmack/ng-redux';
+
+
 import { NgPersonsStore } from '../persons/redux/ng-persons-store';
 import { NgPersonsActions } from '../persons/redux/ng-persons-actions';
 import { NgSkysmackStore } from '../skysmack-core/skysmack/redux/ng-skysmack-store';
-import { MultiSelectFieldComponent } from '@skysmack/portal-ui';
 
 @Injectable({ providedIn: 'root' })
 export class NgPersonsLodgingReservationsFieldProvider extends FieldProvider {
@@ -55,7 +56,7 @@ export class NgPersonsLodgingReservationsFieldProvider extends FieldProvider {
                                         switch (settings.object.shouldBeExistingPersons) {
                                             case true: return [
                                                 new SelectField({
-                                                    component: MultiSelectFieldComponent,
+                                                    // component: MultiSelectFieldComponent,
                                                     value: undefined,
                                                     key: 'extendedData.' + _package.object.path + '.attach',
                                                     optionsData: persons,
@@ -68,7 +69,7 @@ export class NgPersonsLodgingReservationsFieldProvider extends FieldProvider {
                                             ];
                                             case false: return [
                                                 new SelectField({
-                                                    component: MultiSelectFieldComponent,
+                                                    // component: MultiSelectFieldComponent,
                                                     value: undefined,
                                                     key: 'extendedData.' + _package.object.path + '.add',
                                                     optionsData: persons,
@@ -81,7 +82,7 @@ export class NgPersonsLodgingReservationsFieldProvider extends FieldProvider {
                                             ];
                                             default: return [
                                                 new SelectField({
-                                                    component: MultiSelectFieldComponent,
+                                                    // component: MultiSelectFieldComponent,
                                                     value: undefined,
                                                     key: 'extendedData.' + _package.object.path + '.attach',
                                                     optionsData: persons,
@@ -92,7 +93,7 @@ export class NgPersonsLodgingReservationsFieldProvider extends FieldProvider {
                                                     showColumn: true
                                                 }),
                                                 new SelectField({
-                                                    component: MultiSelectFieldComponent,
+                                                    // component: MultiSelectFieldComponent,
                                                     value: undefined,
                                                     key: 'extendedData.' + _package.object.path + '.add',
                                                     optionsData: persons,
