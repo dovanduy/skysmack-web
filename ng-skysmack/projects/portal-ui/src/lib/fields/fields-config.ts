a73ff46a174815b0a1ebb902c09b0c25139c8e63
Martin Jul Hammer <mjh@itinstituttet.dk>
2019-04-02 16:22:07 +0200

    Created converter for dynamic field type to component

--- ng-skysmack/projects/portal-ui/src/lib/fields/fields-config.ts
+++ ng-skysmack/projects/portal-ui/src/lib/fields/fields-config.ts
@@ -1,5 +1,13 @@
 import { LocalObject, FieldSchemaViewModel } from '@skysmack/framework';
 import { EntityFieldsConfig, FormRule, Validation, Field, FieldTypes } from '@skysmack/ng-ui';
+import { IntFieldComponent } from '../components/field-components/components/int-field/int-field.component';
+import { Type } from '@angular/core';
+import { LimitedStringFieldComponent } from '../components/field-components/components/limited-string-field/limited-string-field.component';
+import { GeographyFieldComponent } from '../components/field-components/components/geography-field/geography-field.component';
+import { DoubleFieldComponent } from '../components/field-components/components/double-field/double-field.component';
+import { DecimalFieldComponent } from '../components/field-components/components/decimal-field/decimal-field.component';
+import { DateTimeFieldComponent } from '../components/field-components/components/date-time-field/date-time-field.component';
+import { StringFieldComponent } from '../components/field-components/components/string-field/string-field.component';
 
 export abstract class FieldsConfig<TRecord, TKey, TDependencies> implements EntityFieldsConfig<TRecord, TKey, TDependencies> {
     public abstract formRules: FormRule[];
@@ -31,7 +39,7 @@ export abstract class FieldsConfig<TRecord, TKey, TDependencies> implements Enti
                 ...fields,
                 ...dynamicFields.map(dynamicField => {
                     return new Field({
-                        fieldType: Number(FieldTypes[dynamicField.object.type]),
+                        component: this.getComponentFromDynamicFieldType(dynamicField.object.type),
                         value: entity ? entity.object[dynamicField.object.key] : undefined,
                         key: dynamicField.object.key,
                         label: dynamicField.object.display,
@@ -54,5 +62,18 @@ export abstract class FieldsConfig<TRecord, TKey, TDependencies> implements Enti
         }
 
         return returnfields;
+    }
+
+    protected getComponentFromDynamicFieldType(type: string): Type<any> {
+        switch (type) {
+            case 'int': return IntFieldComponent;
+            case 'string': return StringFieldComponent;
+            case 'dateTime': return DateTimeFieldComponent;
+            case 'decimal': return DecimalFieldComponent;
+            case 'double': return DoubleFieldComponent;
+            case 'geography': return GeographyFieldComponent;
+            case 'limitedString': return LimitedStringFieldComponent;
+            default: console.log('This field type is not defined. Please create a component for it'); return undefined;
+        }
     }
 }
