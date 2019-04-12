36bc0f8aeb36e7f408e23839e849f51330c9c48a
Martin Jul Hammer <mjh@itinstituttet.dk>
2019-04-12 09:35:04 +0200

    Fixed lodging types availability not working.

--- ng-skysmack/projects/ng-packages/src/lib/lodgings/lodging-types/redux/ng-lodgings-types-store.ts
+++ ng-skysmack/projects/ng-packages/src/lib/lodgings/lodging-types/redux/ng-lodgings-types-store.ts
@@ -3,8 +3,8 @@ import { NgRedux } from '@angular-redux/store';
 import { NgRecordStore } from '@skysmack/ng-redux';
 import { LodgingTypesAppState, LodgingType, LODGING_TYPES_AREA_KEY } from '@skysmack/packages-lodgings';
 import { Observable } from 'rxjs';
-import { StrIndex, defined } from '@skysmack/framework';
-import { map } from 'rxjs/operators';
+import { StrIndex, defined, safeUndefinedTo } from '@skysmack/framework';
+import { map, tap } from 'rxjs/operators';
 
 @Injectable({ providedIn: 'root' })
 export class NgLodgingTypesStore extends NgRecordStore<LodgingTypesAppState, LodgingType, number> {
@@ -20,9 +20,10 @@ export class NgLodgingTypesStore extends NgRecordStore<LodgingTypesAppState, Lod
 
     public getAvailableLodgingTypesCount(packagePath: string): Observable<StrIndex<StrIndex<number>>> {
         return this.ngRedux.select(state => state).pipe(
-            map(state => state.lodgingTypes),
-            map(state => state.availableLodgingTypesCount[packagePath]),
-            defined()
+            map(state => state['lodging_types']),
+            map(lodgingTypesState => lodgingTypesState.availableLodgingTypesCount),
+            map(availableLodgingTypesCount => availableLodgingTypesCount[packagePath]),
+            safeUndefinedTo('object')
         );
     }
 }
