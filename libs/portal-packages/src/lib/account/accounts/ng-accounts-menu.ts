7bd7ce13f92c8ebb72cbe09cc1ab8c6f91a9c5a6
Martin Jul Hammer <mjh@itinstituttet.dk>
2019-04-24 15:14:38 +0200

    Account menu now shows (works incorrectly)

---
+++ libs/portal-packages/src/lib/account/accounts/ng-accounts-menu.ts
@@ -0,0 +1,54 @@
+import { Injectable } from '@angular/core';
+import { Router } from '@angular/router';
+import { SidebarMenu } from '@skysmack/portal-ui';
+import { NgSkysmackStore } from '@skysmack/ng-core';
+import { MenuArea } from '@skysmack/framework';
+import { MenuItem } from '@skysmack/framework';
+import { NgMenuItemProviders } from '@skysmack/ng-redux';
+import { ACCOUNT_AREA_KEY } from '@skysmack/packages-account';
+
+
+@Injectable({ providedIn: 'root' })
+export class NgAccountsMenu extends SidebarMenu {
+    public menuId = ACCOUNT_AREA_KEY;
+    public translationPrefix = 'ACCOUNT.INDEX.';
+
+    constructor(
+        public store: NgSkysmackStore,
+        public router: Router,
+        public menuItemProviders: NgMenuItemProviders
+    ) {
+        super(store, router, menuItemProviders);
+        this.setPrimaryMenu();
+        this.setSpeedDialMenu();
+        this.runMenuItemProviders();
+    }
+
+    public setPrimaryMenu() {
+        this.primaryMenuAreas.push(new MenuArea({
+            area: 'actions',
+            translationPrefix: this.translationPrefix,
+            order: 1,
+        }));
+
+        this.primaryMenuItems.push(new MenuItem({
+            url: 'create',
+            displayName: this.translationPrefix + 'CHANGE_PASSWORD',
+            area: 'actions',
+            order: 1,
+            icon: 'groupAdd',
+        }));
+    }
+
+    public setSpeedDialMenu() {
+        this.speedDialMenu = [
+            new MenuItem({
+                url: 'create',
+                displayName: this.translationPrefix + 'CREATE',
+                area: undefined,
+                order: 1,
+                icon: 'add',
+            }),
+        ];
+    }
+}
