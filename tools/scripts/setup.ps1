ae5746c73df2e37f7898e4dd04107c9260a6b403
Martin Jul Hammer <mjh@itinstituttet.dk>
2019-05-02 16:51:49 +0200

    Incommented outcommented code in setup.

--- tools/scripts/setup.ps1
+++ tools/scripts/setup.ps1
@@ -4,10 +4,10 @@ write-host "Setting up Skysmack local development"
 write-host "----------------------------"
 
 ### NodeJS
-# & "$PSScriptRoot\setup\install-nodejs.ps1"
+& "$PSScriptRoot\setup\install-nodejs.ps1"
 
 ### NPM packages
-# & "$PSScriptRoot\setup\install-skysmack-global-npm-packages.ps1"
+& "$PSScriptRoot\setup\install-skysmack-global-npm-packages.ps1"
 
 write-host "Done! To start the local dev env, run: C:\Users\<USERNAME>\Source\Repos\skysmack-api\tools\start-frontend.ps1 - Remember to setup and seed the backend first."
 write-host ""
