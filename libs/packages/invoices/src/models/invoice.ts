35b57768902ae13da9220320c969cf872b96e4f7
Morten Aslo-Petersen <morten@itinstituttet.dk>
2019-05-06 17:31:52 +0200

    Invoice model extended with dat about total, payment, items etc.

--- libs/packages/invoices/src/models/invoice.ts
+++ libs/packages/invoices/src/models/invoice.ts
@@ -3,6 +3,14 @@ import { DocumentRecord } from "@skysmack/framework";
 export class Invoice extends DocumentRecord<number> {
     public currencyCode: number;
 
+    public status: 'draft' | 'processing' | 'invoiced';
+    public paid: boolean;
+    public total: number;
+    public totalTax: number;
+    public balance: number;
+    public itemIds: number[];
+    public paymentIds: number[];
+
     public constructor(init?: Partial<Invoice>) {
         super();
         Object.assign(this, init);
