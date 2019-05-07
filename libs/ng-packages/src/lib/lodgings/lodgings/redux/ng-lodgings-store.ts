168de1cef4b9ed08c42b8f02f2ab1c787879255e
Martin Jul Hammer <mjh@itinstituttet.dk>
2019-05-07 15:10:56 +0200

    getSingle with dependencies now works for products and lodgings.

--- libs/ng-packages/src/lib/lodgings/lodgings/redux/ng-lodgings-store.ts
+++ libs/ng-packages/src/lib/lodgings/lodgings/redux/ng-lodgings-store.ts
@@ -1,14 +1,22 @@
 import { Injectable } from '@angular/core';
 import { NgRedux } from '@angular-redux/store';
-import { Lodging, LodgingsAppState, LODGINGS_AREA_KEY } from '@skysmack/packages-lodgings';
+import { Lodging, LodgingsAppState } from '@skysmack/packages-lodgings';
 import { NgRecordStore } from '@skysmack/ng-redux';
 import { Observable } from 'rxjs';
-import { StrIndex, defined } from '@skysmack/framework';
+import { StrIndex, defined, LocalObject } from '@skysmack/framework';
 import { map } from 'rxjs/operators';
 
 @Injectable({ providedIn: 'root' })
 export class NgLodgingsStore extends NgRecordStore<LodgingsAppState, Lodging, number> {
     constructor(protected ngRedux: NgRedux<LodgingsAppState>) { super(ngRedux, 'lodgings'); }
+
+    public get(packagePath: string): Observable<LocalObject<Lodging, number>[]> {
+        return this.getWithDependencies(packagePath, 'lodgingType', 'lodgingTypeId', 'lodgingTypes');
+    }
+
+    public getSingle(packagePath: string, id: number): Observable<LocalObject<Lodging, number>> {
+        return this.getSingleWithDependencies(packagePath, id, 'lodgingType', 'lodgingTypeId', 'lodgingTypes');
+    }
 
     public getAvailableLodgings(packagePath: string): Observable<StrIndex<number[]>> {
         return this.ngRedux.select(state => state).pipe(
--- libs/ng-packages/src/lib/products/products/redux/ng-products-store.ts
+++ libs/ng-packages/src/lib/products/products/redux/ng-products-store.ts
@@ -1,41 +1,19 @@
 import { Injectable } from '@angular/core';
 import { NgRedux } from '@angular-redux/store';
-import { Product, ProductsAppState, ProductType, ProductTypesState } from '@skysmack/packages-products';
+import { Product, ProductsAppState } from '@skysmack/packages-products';
 import { NgRecordStore } from '@skysmack/ng-redux';
-import { LocalObject, hasValue, safeUndefinedTo, dictionaryToArray } from '@skysmack/framework';
-import { Observable, combineLatest } from 'rxjs';
-import { map } from 'rxjs/operators';
+import { LocalObject } from '@skysmack/framework';
+import { Observable } from 'rxjs';
 
 @Injectable({ providedIn: 'root' })
 export class NgProductsStore extends NgRecordStore<ProductsAppState, Product, number> {
     constructor(protected ngRedux: NgRedux<ProductsAppState>) { super(ngRedux, 'products'); }
 
     public get(packagePath: string): Observable<LocalObject<Product, number>[]> {
-        return combineLatest(super.get(packagePath), this.getProductTypes(packagePath)).pipe(
-            map(values => {
-                const products = values[0];
-                const productTypes = values[1];
-                for (let index = 0; index < products.length; index++) {
-                    const product = products[index];
-                    if (product.object.productTypeId && product.object.productTypeId > 0) {
-                        product.object.productType = productTypes.find(productType => productType.object.id === product.object.productTypeId);
-                    }
-                }
-                return products;
-            })
-        );
+        return this.getWithDependencies(packagePath, 'productType', 'productTypeId', 'productTypes');
     }
 
-    private getProductTypes(packagePath: string): Observable<LocalObject<ProductType, number>[]> {
-        return this.getProductTypeState().pipe(
-            map(state => state.localRecords[packagePath]),
-            safeUndefinedTo('object'),
-            dictionaryToArray<LocalObject<ProductType, number>>(),
-            hasValue()
-        );
-    }
-
-    protected getProductTypeState(): Observable<ProductTypesState> {
-        return this.store.select(state => state['productTypes']);
+    public getSingle(packagePath: string, id: number): Observable<LocalObject<Product, number>> {
+        return this.getSingleWithDependencies(packagePath, id, 'productType', 'productTypeId', 'productTypes');
     }
 }
