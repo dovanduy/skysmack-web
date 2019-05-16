ba121abe4aac7cbff0550a80df515b71e82bab78
Martin Jul Hammer <mjh@itinstituttet.dk>
2019-05-16 11:33:41 +0200

    Added epic that updates skysmack tenant when skysmack settings are changed.

--- libs/ng-core/src/lib/packages/redux/ng-packages-epics.ts
+++ libs/ng-core/src/lib/packages/redux/ng-packages-epics.ts
@@ -1,7 +1,7 @@
 import { ofType, ActionsObservable, Epic } from 'redux-observable';
-import { ReduxAction, PackagePathPayload, GetAvailablePackagesSuccessPayload, CommitMeta, RollbackMeta, GetPagedEntitiesPayload, GetPagedEntitiesSuccessPayload, ReduxOfflineMeta, QueueActions, CancelActionPayload } from '@skysmack/redux';
+import { ReduxAction, PackagePathPayload, GetAvailablePackagesSuccessPayload, CommitMeta, RollbackMeta, GetPagedEntitiesPayload, GetPagedEntitiesSuccessPayload, ReduxOfflineMeta, QueueActions, CancelActionPayload, SettingsCommitMeta } from '@skysmack/redux';
 import { map, mergeMap } from 'rxjs/operators';
-import { Observable } from 'rxjs';
+import { Observable, of } from 'rxjs';
 import { Injectable } from '@angular/core';
 import { NgPackagesRequests } from './ng-packages-requests';
 import { GetPackageSuccessPayload } from '@skysmack/packages-skysmack-core';
@@ -10,6 +10,7 @@ import { HttpErrorResponse, LocalObject, Package, HttpResponse, QueueItem, StrIn
 import { NgPackagesNotifications } from '../ng-packages-notifications';
 import { NgSkysmackActions } from '../../skysmack/redux/ng-skysmack-actions';
 import { NgSkysmackRequests } from '../../skysmack/redux/ng-skysmack-requests';
+import { NgSettingsActions } from '@skysmack/ng-redux';
 
 @Injectable({ providedIn: 'root' })
 export class NgPackagesEpics {
@@ -36,8 +37,10 @@ export class NgPackagesEpics {
             this.successActionEpic,
             this.failureActionEpic,
             this.cancelActionEpic,
+            // Additional skysmack epics
             this.getPermissionsEpic,
-            this.getAvailablePermissionsEpic
+            this.getAvailablePermissionsEpic,
+            this.getSkysmackOnSettingsUpdateEpic
         ];
     }
 
@@ -198,6 +201,7 @@ export class NgPackagesEpics {
     //#endregion
 
     // ADDITIONAL SKYSMACK EPICS
+    // These are added here, since the ng-skysmack-epics.ts file only accepts one epic for some reason...
     public getPermissionsEpic = (action$: ActionsObservable<ReduxAction<string>>): Observable<ReduxAction<string[]> | ReduxAction<HttpErrorResponse>> => {
         return action$.pipe(
             ofType(NgSkysmackActions.GET_PACKAGE_PERMISSIONS),
@@ -209,6 +213,20 @@ export class NgPackagesEpics {
         return action$.pipe(
             ofType(NgSkysmackActions.GET_AVAILABLE_PACKAGE_PERMISSIONS),
             mergeMap(action => this.skysmackRequests.getAvailablePermissions(action as any))
+        );
+    }
+
+    public getSkysmackOnSettingsUpdateEpic = (action$: ActionsObservable<ReduxAction<string>>) => {
+        return action$.pipe(
+            ofType(NgSettingsActions.UPDATE_SETTINGS_SUCCESS),
+            mergeMap(action => {
+                const castedAction = action as ReduxAction<any, SettingsCommitMeta<LocalObject<any, unknown>>>;
+                if (castedAction.meta.stateKey === 'skysmack') {
+                    return this.skysmackRequests.get();
+                } else {
+                    return of({ type: 'IGNORE_THIS_ACTION' });
+                }
+            })
         );
     }
 }
