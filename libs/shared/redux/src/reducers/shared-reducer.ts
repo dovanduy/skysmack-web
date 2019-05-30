a16c60a7ff303f7b9359c62b0b4b0d412066cdb2
Martin Jul Hammer <mjh@itinstituttet.dk>
2019-05-30 17:07:13 +0200

    Started updating sharedReducer to be able to reinstantiate local objects for more than one property per state.

--- libs/shared/redux/src/reducers/shared-reducer.ts
+++ libs/shared/redux/src/reducers/shared-reducer.ts
@@ -2,6 +2,18 @@ import { AuthenticationActions } from '../authentication/authentication-actions'
 import { REHYDRATE } from 'redux-persist/constants';
 import { LocalObject, toLocalObject, StrIndex } from '@skysmack/framework';
 
+const reinstantiateLocalRecord = (localRecord: LocalObject<any, any>) => {
+    return localRecord.objectIdentifier ? localRecord : toLocalObject(
+        localRecord.object,
+        localRecord._identifier,
+        localRecord.localId,
+        localRecord.status,
+        localRecord.modifyType,
+        localRecord.isNew,
+        localRecord.foreignKey,
+        localRecord.error
+    );
+}
 
 export function sharedReducer(state: any, action: any, initialState: any, reduxArea: string): any {
     state = Object.freeze(state);
@@ -9,23 +21,26 @@ export function sharedReducer(state: any, action: any, initialState: any, reduxA
     switch (action.type) {
         case REHYDRATE: { // Reinstantiate localRecords so their functions gets defined agian.
             const newState = { ...action.payload[reduxArea] };
-            if (newState && newState.localRecords) {
-                Object.keys(newState.localRecords).map(packagePath => {
-                    const localRecords: StrIndex<LocalObject<any, any>> = newState.localRecords[packagePath];
-                    Object.keys(localRecords).map(localRecordKey => {
-                        const localRecord: LocalObject<any, any> = localRecords[localRecordKey];
-                        const initiatedLocalRecord = localRecord.objectIdentifier ? localRecord : toLocalObject(
-                            localRecord.object,
-                            localRecord._identifier,
-                            localRecord.localId,
-                            localRecord.status,
-                            localRecord.modifyType,
-                            localRecord.isNew,
-                            localRecord.foreignKey,
-                            localRecord.error
-                        );
-                        localRecords[localRecordKey] = initiatedLocalRecord
-                    });
+            const propNames = [ // Make into sharedReducer param, then update AAAAAAALL the reducers ;_; - or make a default :D
+                'localRecords'
+            ];
+
+            const localObjectsToReinstantiate = Object.keys(newState).map(key => {
+                return propNames.includes(key) ? key : undefined
+            }).filter(x => x);
+
+            if (newState && localObjectsToReinstantiate.length > 0) {
+                localObjectsToReinstantiate.map(propName => {
+                    if (propName === 'localRecords') {
+                        Object.keys(newState.localRecords).map(packagePath => {
+                            const localRecords: StrIndex<LocalObject<any, any>> = newState.localRecords[packagePath];
+                            Object.keys(localRecords).map(localRecordKey => {
+                                localRecords[localRecordKey] = reinstantiateLocalRecord(localRecords[localRecordKey])
+                            });
+                        });
+                    } else {
+                        // If not localRecords
+                    }
                 });
 
                 return { ...state, ...newState };
