b426b52a15eee1dfdf4880acca94e42f30f84476
Martin Jul Hammer <mjh@itinstituttet.dk>
2019-06-11 14:50:20 +0200

    Removed deprecated path

--- libs/portal-packages/src/lib/account/accounts/components/accounts-components.ts
+++ libs/portal-packages/src/lib/account/accounts/components/accounts-components.ts
@@ -2,7 +2,6 @@ import { Routes } from '@angular/router';
 import { ChangePasswordComponent } from './change-password/change-password.component';
 import { AccountComponent } from './account/account.component';
 import { ConfirmEmailComponent } from './confirm-email/confirm-email.component';
-import { VerifyEmailComponent } from './verify-email/verify-email.component';
 import { ForgotPasswordComponent } from './forgot-password/forgot-password.component';
 import { RecoverPasswordComponent } from './recover-password/recover-password.component';
 
