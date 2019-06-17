47fe8b7c4af099a9f47b7784f6f175f2d311b577
Martin Jul Hammer <mjh@itinstituttet.dk>
2019-06-17 10:59:59 +0200

    - Added smtp client settings model - Removed unused type from persons lodging reservations.

--- libs/packages/emails-smtp/src/index.ts
+++ libs/packages/emails-smtp/src/index.ts
@@ -1,1 +1,3 @@
+export * from './models/index';
+
 export * from './emails-smtp-type';
---
+++ libs/packages/emails-smtp/src/models/smtp-client-settings.ts
@@ -0,0 +1,7 @@
+export class SmtpClientSettings {
+    public host: string;
+    public port: number;
+    public username: string;
+    public password: string;
+    public enableSsl: boolean;
+}
