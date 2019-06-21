aba52fd567880ae1ead8a8b8f4fb5616db1b3d4b
Martin Jul Hammer <mjh@itinstituttet.dk>
2019-06-21 15:18:38 +0200

    Setup connection entity action

--- libs/portal-packages/src/lib/terminal-payments/connections/components/connections-index/connections-index.component.ts
+++ libs/portal-packages/src/lib/terminal-payments/connections/components/connections-index/connections-index.component.ts
@@ -1,13 +1,15 @@
 import { Component, OnInit } from '@angular/core';
 import { EntityComponentPageTitle, EntityActionProviders, ENTITY_ACTIONS_DELETE, RecordIndexComponent } from '@skysmack/portal-ui';
 import { Router, ActivatedRoute } from '@angular/router';
-import { ConnectionsAppState, Connection, CONNECTIONS_AREA_KEY, ConnectionKey } from '@skysmack/packages-terminal-payments';
+import { ConnectionsAppState, Connection, CONNECTIONS_AREA_KEY, ConnectionKey, Terminal, TerminalStatus } from '@skysmack/packages-terminal-payments';
 import { NgConnectionsMenu } from '../../ng-connections-menu';
 import { EntityAction } from '@skysmack/ng-ui';
 import { NgConnectionsFieldsConfig } from '../../ng-connections-fields-config';
 import { NgSkysmackStore } from '@skysmack/ng-core';
 import { NgConnectionsActions, NgConnectionsStore } from '@skysmack/ng-packages';
 import { NgSignalR } from '@skysmack/ng-framework';
+import { HttpClient } from '@angular/common/http';
+import { LocalObject } from '@skysmack/framework';
 
 @Component({
   selector: 'ss-connections-index',
@@ -17,7 +19,13 @@ export class ConnectionsIndexComponent extends RecordIndexComponent<ConnectionsA
   public areaKey: string = CONNECTIONS_AREA_KEY;
   public titleExtras = true;
   public entityActions: EntityAction[] = [
-    // new EntityAction().asUrlAction('edit', ENTITY_ACTIONS_EDIT, 'edit'),
+    new EntityAction().asEventAction('Connect', this.connection, 'add', this).setShowLogic((entity: LocalObject<Connection, ConnectionKey>) => {
+      if (entity.object.status === TerminalStatus.Unknown) {
+        return true;
+      } else if (entity.object.status === TerminalStatus.Unavailable) {
+        return true;
+      } //... etc
+    }),
     new EntityAction().asEventAction(ENTITY_ACTIONS_DELETE, this.delete, 'delete', this)
   ];
 
@@ -31,7 +39,8 @@ export class ConnectionsIndexComponent extends RecordIndexComponent<ConnectionsA
     public fieldsConfig: NgConnectionsFieldsConfig,
     public title: EntityComponentPageTitle,
     public entityActionProviders: EntityActionProviders,
-    public signalR: NgSignalR
+    public signalR: NgSignalR,
+    public httpClient: HttpClient
   ) {
     super(router, activatedRoute, actions, redux, store, fieldsConfig, entityActionProviders, title);
   }
@@ -39,5 +48,9 @@ export class ConnectionsIndexComponent extends RecordIndexComponent<ConnectionsA
   ngOnInit() {
     super.ngOnInit();
     this.signalR.instance.join(this.packagePath);
+  }
+
+  protected connection(value: LocalObject<Connection, ConnectionKey>, _this: RecordIndexComponent<any, any, any>) {
+    console.log('hello', value);
   }
 }
