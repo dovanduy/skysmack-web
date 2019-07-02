73434e85dc2f6600035cbc68d6914b6817025276
Martin Jul Hammer <mjh@itinstituttet.dk>
2019-07-02 14:55:19 +0200

    MenuItem now has same functionality EntityAction

--- libs/shared/framework/src/models/menu-item.ts
+++ libs/shared/framework/src/models/menu-item.ts
@@ -1,16 +1,64 @@
-import { DocumentRecord } from "./document-record";
+export class MenuItem {
+    public showLogic: Function;
 
-export class MenuItem extends DocumentRecord<number>{
+    // Event Action Props
+    public isActionEvent: boolean;
+    public action: Function;
+    public _this: any;
+
+    // URL Action Props
     public url: string;
+    public urlPostfix: string;
     public displayName: string;
     public area: string;
     public order: number;
     public icon: string;
-    public permissions?: string[]
+    public permissions: string[];
     public display = true;
 
     public constructor(init?: Partial<MenuItem>) {
-        super();
         Object.assign(this, init);
     }
+    /**
+     * Currently "only works" with edit and delete actions.
+     * To be precise, the given url is used and gets the entity id/key appended.
+     */
+    public asUrlAction(url: string, displayName: string, icon: string, urlPostfix: string = ''): MenuItem {
+        Object.assign(this, { isActionEvent: false, url, displayName, icon, urlPostfix: urlPostfix });
+        return this;
+    }
+
+    public asEventAction(displayName: string, action: Function, icon: string, _this?: any): MenuItem {
+        Object.assign(this, { isActionEvent: true, displayName, action, icon, _this });
+        return this;
+    }
+
+    public setPermissions(permissions: string[]): MenuItem {
+        this.permissions = permissions;
+        return this;
+    }
+
+    public setShowLogic(showLogic: Function) {
+        this.showLogic = showLogic;
+        return this;
+    }
+
+    public runShowLogic(entity): boolean {
+        return this.showLogic ? this.showLogic(entity) : true;
+    }
 }
+
+// Old MenuItem. Kept as temp reference. If you think this has no further use, feel free to delete it.
+// export class MenuItem {
+//     public url: string;
+//     public displayName: string;
+//     public area: string;
+//     public order: number;
+//     public icon: string;
+//     public permissions?: string[]
+//     public display = true;
+
+//     public constructor(init?: Partial<MenuItem>) {
+//         Object.assign(this, init);
+//     }
+// }
