f0f42d841bd5985391f2518b2183fd01bb1b5bf3
Christian Vilen <christian-vilen@itinstituttet.dk>
2019-07-12 13:54:52 +0200

    Revert "New structure for ng-access-policies"
    This reverts commit dc3c5ffdb62d73240e0f12b96488383c4c48d21f.

--- libs/ng-packages/ng-access-policies/README.md
+++
@@ -1,7 +0,0 @@
-# ng-access-policies
-
-This library was generated with [Nx](https://nx.dev).
-
-## Running unit tests
-
-Run `ng test ng-access-policies` to execute the unit tests.
--- libs/ng-packages/ng-access-policies/jest.config.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-  name: 'ng-access-policies',
-  preset: '../../../jest.config.js',
-  coverageDirectory: '../../../coverage/libs/ng-packages/ng-access-policies',
-  snapshotSerializers: [
-    'jest-preset-angular/AngularSnapshotSerializer.js',
-    'jest-preset-angular/HTMLCommentSerializer.js'
-  ]
-};
