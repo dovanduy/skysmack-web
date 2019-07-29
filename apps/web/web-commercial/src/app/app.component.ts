e3816f5b29f02f85e1491ee0c66c7dc23c23196d
Morten Aslo-Petersen <morten@itinstituttet.dk>
2019-07-29 16:19:21 +0200

    Commercial site updates

--- apps/web/web-commercial/src/app/app.component.ts
+++ apps/web/web-commercial/src/app/app.component.ts
@@ -1,7 +1,7 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'skysmack-root',
+  selector: 'skysmack-app',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
--- apps/web/web-commercial/src/app/app.module.ts
+++ apps/web/web-commercial/src/app/app.module.ts
@@ -3,12 +3,14 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { RouterModule } from '@angular/router';
+import { MaterialModule } from '@skysmack/portal-ui';
 
 @NgModule({
   declarations: [AppComponent],
   imports: [
     BrowserModule,
-    RouterModule.forRoot([], { initialNavigation: 'enabled' })
+    RouterModule.forRoot([], { initialNavigation: 'enabled' }),    
+    MaterialModule
   ],
   providers: [],
   bootstrap: [AppComponent]
