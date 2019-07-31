14097e7ef32ece8eb22aebac8eb5ab4ca40ac0bd
Martin Jul Hammer <mjh@itinstituttet.dk>
2019-07-31 11:24:24 +0200

    Updated skysmack-commercial-api-domain

--- apps/web/web-commercial/src/requests/skysmack-commercial-api-domain.ts
+++ apps/web/web-commercial/src/requests/skysmack-commercial-api-domain.ts
@@ -1,52 +1,16 @@
 import { Injectable } from '@angular/core';
 import { environment } from '../environments/environment';
-import { ApiDomain, Url } from '@skysmack/framework';
+import { ApiDomain } from '@skysmack/framework';
 
 @Injectable({ providedIn: 'root' })
 export class SkysmackCommercialApiDomain implements ApiDomain {
     public domain: string;
 
     constructor() {
-        this.domain = this.getApiDomain();
-    }
-
-    private getApiDomain(): string {
-        let apiDomain = '';
-
-        // Local production testing
-        const localApi = 'http://localhost:52678';
-        const localProdClient = 'localhost:4200';
-
-        const devHostClient = 'skysmack.test';
-        const devPortalHostApi = 'localhost:52678';
-        const devTenantsHostApi = 'localhost:52678';
-
-        // Production builds
-        const prodHostClient = 'skysmack.net';
-        const prodHostApi = 'skysmack.io';
-
-        const urlInfo = new Url();
-
-        const notLocalTesting = urlInfo.host !== localProdClient;
-
-        if (environment.production && notLocalTesting) {
-            if (urlInfo.noPortHost.endsWith(prodHostClient) && urlInfo.subdomain.length > 0) {
-                apiDomain = 'https://' + urlInfo.subdomain + '.' + prodHostApi;
-            } else {
-                apiDomain = 'https://' + prodHostApi;
-            }
+        if (environment.production) {
+            this.domain = 'https://partners.skysmack.com';
         } else {
-            if (urlInfo.noPortHost.endsWith(devHostClient)) {
-                if (urlInfo.subdomain.length > 0) {
-                    apiDomain = urlInfo.httpProtocol + urlInfo.subdomain + '.' + devPortalHostApi;
-                } else {
-                    apiDomain = urlInfo.httpProtocol + devTenantsHostApi;
-                }
-            } else {
-                apiDomain = localApi;
-            }
+            this.domain = 'http://localhost:52678';
         }
-
-        return apiDomain;
     }
 }
