918651dc7d813bfb11eb375fbf70aa4e065248a6
Christian Vilen <christian-vilen@itinstituttet.dk>
2019-08-01 14:09:04 +0200

    Import fix for oauth2

--- libs/portal-packages/src/lib/oauth2/index.ts
+++ libs/portal-packages/src/lib/oauth2/index.ts
@@ -3,4 +3,5 @@ export * from './oauth2-routing.module';
 export * from './oauth2.module';
 export * from './login-validation';
 export * from './login-fields-config';
-export * from './oauth2-menu-provider';
+export * from './ng-oauth2-menu';
+export * from './ng-oauth2-menu-provider';
