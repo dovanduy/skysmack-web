0538e2846057477d8429810541222aab63fef0b1
Christian Vilen <christian-vilen@itinstituttet.dk>
2019-08-07 14:29:01 +0200

    Removed sidebar menu flow

--- libs/portal-ui/src/lib/components/common/sidebar-menu/sidebar-menu.component.ts
+++ libs/portal-ui/src/lib/components/common/sidebar-menu/sidebar-menu.component.ts
@@ -2,8 +2,7 @@ import { Component, Input, OnInit, Output, EventEmitter } from '@angular/core';
 import { Router, ActivatedRoute } from '@angular/router';
 import { TranslateService } from '@ngx-translate/core';
 import { trigger, state, style, transition, animate } from '@angular/animations';
-import { SidebarMenu } from './../../../models/sidebar-menu/sidebar-menu';
-import { MenuItem, MenuAreaItems } from '@skysmack/framework';
+import { MenuAreaItems } from '@skysmack/framework';
 import { NgMenuProviders } from '../../../navigation/ng-menu-providers';
 import { Observable } from 'rxjs';
 import { map } from 'rxjs/operators';
@@ -21,7 +20,6 @@ import { map } from 'rxjs/operators';
   ],
 })
 export class SidebarMenuComponent implements OnInit {
-  @Input() public sidebarMenu: SidebarMenu;
   @Input() public componentKey: string;
   @Output() public menuItemActionEvent = new EventEmitter<any>();
   public expansions: any = {};
@@ -37,27 +35,11 @@ export class SidebarMenuComponent implements OnInit {
   }
 
   ngOnInit() {
-    this.removeEmptyMenuAreas();
-
     const packagePath = this.router.url.split('/')[1];
     this.menuAreaItems$ = this.ngMenuProviders.getMenuAreaItems(packagePath, this.componentKey).pipe(
       map(menuAreaItems => {
         return menuAreaItems.filter(menuAreaItem => menuAreaItem && menuAreaItem.providedIn && menuAreaItem.providedIn.includes('sidebar'));
       })
     );
-  }
-
-  public permissionsChecked(displaying: boolean, menuItem: MenuItem) {
-    menuItem.display = displaying;
-    const currentValues = this.sidebarMenu.primaryMenuAreas$.getValue();
-    const menuArea = currentValues.find(x => x.area === menuItem.area);
-    menuArea.display = currentValues.filter(x => x.area === menuItem.area && x.display).length > 0;
-  }
-
-  public removeEmptyMenuAreas() {
-    const menuAreas = this.sidebarMenu.primaryMenuAreas$.getValue();
-    const menuItems = this.sidebarMenu.primaryMenuItems$.getValue();
-    const currentValues = menuAreas.filter(menuArea => menuItems.find(menuItem => menuItem.area === menuArea.area) ? true : false)
-    this.sidebarMenu.primaryMenuAreas$.next(currentValues);
   }
 }
