2662036a85a73317af986e02cffe4e9271eadbb1
Martin Jul Hammer <mjh@itinstituttet.dk>
2019-08-08 08:33:09 +0200

    Removed lodging reservations menu item provider.

--- libs/ng-packages/ng-lodging-reservations/src/lib/index.ts
+++ libs/ng-packages/ng-lodging-reservations/src/lib/index.ts
@@ -1,4 +1,3 @@
 export * from './lodging-reservations/index';
 export * from './ng-lodging-reservations.module';
-export * from './ng-lodgings-reservations-menu-item-provider';
 export * from './ng-lodging-reservations-settings-validation';
