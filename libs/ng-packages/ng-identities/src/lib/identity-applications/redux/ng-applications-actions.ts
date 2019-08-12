3c928c04f99888a44736de3b494a7ea0e6cde304
Christian Vilen <christian-vilen@itinstituttet.dk>
2019-08-12 16:16:03 +0200

    Queue messages for identities

--- libs/ng-packages/ng-identities/src/lib/identity-applications/redux/ng-applications-actions.ts
+++ libs/ng-packages/ng-identities/src/lib/identity-applications/redux/ng-applications-actions.ts
@@ -1,8 +1,15 @@
 import { NgRedux } from '@angular-redux/store';
 import { Injectable } from '@angular/core';
-import { ApplicationsAppState, ApplicationsActions } from '@skysmack/packages-identities';
+import { ApplicationsAppState, ApplicationsActions, Application } from '@skysmack/packages-identities';
+import { LocalObject, StrIndex } from '@skysmack/framework';
 
 @Injectable({ providedIn: 'root' })
 export class NgApplicationsActions extends ApplicationsActions {
     constructor(protected store: NgRedux<ApplicationsAppState>) { super(store); }
+
+    public getMessageParams(record: LocalObject<Application, number>): StrIndex<string> {
+        return {
+            displayName: record.object.displayName
+        };
+    }
 }
