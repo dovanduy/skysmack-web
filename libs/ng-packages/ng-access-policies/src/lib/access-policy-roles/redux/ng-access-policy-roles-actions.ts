25923252ea4f7c292f61e9db061c540b7684b8fe
Christian Vilen <christian-vilen@itinstituttet.dk>
2019-08-12 15:41:41 +0200

    Queue messages for access  policies

--- libs/ng-packages/ng-access-policies/src/lib/access-policy-roles/redux/ng-access-policy-roles-actions.ts
+++ libs/ng-packages/ng-access-policies/src/lib/access-policy-roles/redux/ng-access-policy-roles-actions.ts
@@ -1,8 +1,15 @@
 import { NgRedux } from '@angular-redux/store';
 import { Injectable } from '@angular/core';
-import { AccessPolicyRolesAppState, AccessPolicyRolesActions } from '@skysmack/packages-skysmack-core';
+import { AccessPolicyRolesAppState, AccessPolicyRolesActions, AccessPolicyRole, AccessPolicyRoleKey } from '@skysmack/packages-skysmack-core';
+import { LocalObject, StrIndex } from '@skysmack/framework';
 
 @Injectable({ providedIn: 'root' })
 export class NgAccessPolicyRolesActions extends AccessPolicyRolesActions {
     constructor(protected store: NgRedux<AccessPolicyRolesAppState>) { super(store); }
+
+    public getMessageParams(record: LocalObject<AccessPolicyRole, AccessPolicyRoleKey>): StrIndex<string> {
+        return {
+            id: record.object.id.toString()
+        };
+    }
 }
--- libs/ng-packages/ng-access-policies/src/lib/access-policy-rules/redux/ng-access-policy-rules-actions.ts
+++ libs/ng-packages/ng-access-policies/src/lib/access-policy-rules/redux/ng-access-policy-rules-actions.ts
@@ -1,8 +1,15 @@
 import { NgRedux } from '@angular-redux/store';
 import { Injectable } from '@angular/core';
-import { AccessPolicyRulesAppState, AccessPolicyRulesActions } from '@skysmack/packages-skysmack-core';
+import { AccessPolicyRulesAppState, AccessPolicyRulesActions, AccessPolicyRule } from '@skysmack/packages-skysmack-core';
+import { LocalObject, StrIndex } from '@skysmack/framework';
 
 @Injectable({ providedIn: 'root' })
 export class NgAccessPolicyRulesActions extends AccessPolicyRulesActions {
     constructor(protected store: NgRedux<AccessPolicyRulesAppState>) { super(store); }
+
+    public getMessageParams(record: LocalObject<AccessPolicyRule, number>): StrIndex<string> {
+        return {
+            id: record.object.id.toString()
+        };
+    }
 }
