0c40ca0f1d535f2ae37a290275d0be649157f82d
Martin Jul Hammer <mjh@itinstituttet.dk>
2019-08-13 08:44:19 +0200

    Commercial: Lazy loading now works.

--- apps/web/web-commercial/src/app/app.module.ts
+++ apps/web/web-commercial/src/app/app.module.ts
@@ -14,11 +14,9 @@ import { NgOAuth2Module } from '@skysmack/ng-oauth2';
 import { MaterialModule } from '@skysmack/portal-ui';
 
 import { AppComponent } from './app.component';
-import { commercialAccountRoute } from './packages/commercial_account_wrapper.module';
 import { environment } from '../environments/environment';
 import { HttpClientModule } from '@angular/common/http';
 import { commercialApplicationStartup } from './commercial-application-startup';
-import { commercialTenantsRoute } from './packages/commercial_tenants_wrapper.module';
 import { StartComponent } from './pages/start/start.component';
 import { WhyComponent } from './pages/why/why.component';
 import { SolutionsComponent } from './pages/solutions/solutions.component';
@@ -26,14 +24,11 @@ import { ProductsComponent } from './pages/products/products.component';
 import { PricingsComponent } from './pages/pricings/pricings.component';
 import { GettingStartedComponent } from './pages/getting-started/getting-started.component';
 import { NgTranslationModule, LanguageService } from '@skysmack/ng-translation';
-import { commercialUsersRoute } from './packages/commercial_users_wrapper.module';
 import { AuthenticatedLoadStrategy } from './authenticated-load-strategy';
 
-const lazyLoadedPacakges = [
-  commercialAccountRoute,
-  commercialTenantsRoute,
-  commercialUsersRoute
-];
+import { commercialAccountRoute } from './packages/commercial_account_wrapper.module';
+import { commercialUsersRoute } from './packages/commercial_users_wrapper.module';
+import { commercialTenantsRoute } from './packages/commercial_tenants_wrapper.module';
 
 @NgModule({
   declarations: [AppComponent, StartComponent, WhyComponent, SolutionsComponent, ProductsComponent, PricingsComponent, GettingStartedComponent],
@@ -43,6 +38,12 @@ const lazyLoadedPacakges = [
     NgOAuth2Module,
     NgTranslationModule,
     RouterModule.forRoot([
+      // commercialAccountRoute,
+      // commercialTenantsRoute,
+      // commercialUsersRoute,
+      { path: 'account', loadChildren: './packages/commercial_account_wrapper.module#CommercialAccountWrapperModule' },
+      { path: 'tenants', loadChildren: './packages/commercial_tenants_wrapper.module#CommercialTenantsWrapperModule' },
+      { path: 'users', loadChildren: './packages/commercial_users_wrapper.module#CommercialUsersWrapperModule' },
       {
         path: '',
         component: StartComponent
@@ -67,7 +68,6 @@ const lazyLoadedPacakges = [
         path: 'getting-started',
         component: GettingStartedComponent
       },
-      ...lazyLoadedPacakges
     ], { initialNavigation: 'enabled', preloadingStrategy: AuthenticatedLoadStrategy }),
     BrowserAnimationsModule,
     MaterialModule,
