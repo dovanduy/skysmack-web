27c6ddc2f8e8f2e6f0ae7e52dbbc866f3bcd1d98
Christian Vilen <christian-vilen@itinstituttet.dk>
2019-08-13 10:57:48 +0200

    Queue messages for for product pricings

--- libs/ng-packages/ng-products-pricings/src/lib/product-type-price-changes/redux/ng-product-type-price-changes-actions.ts
+++ libs/ng-packages/ng-products-pricings/src/lib/product-type-price-changes/redux/ng-product-type-price-changes-actions.ts
@@ -1,8 +1,15 @@
 import { NgRedux } from '@angular-redux/store';
 import { Injectable } from '@angular/core';
-import { ProductTypePriceChangesAppState, ProductTypePriceChangesActions } from '@skysmack/packages-products-pricings';
+import { ProductTypePriceChangesAppState, ProductTypePriceChangesActions, ProductTypePriceChange } from '@skysmack/packages-products-pricings';
+import { LocalObject, StrIndex } from '@skysmack/framework';
 
 @Injectable({ providedIn: 'root' })
 export class NgProductTypePriceChangesActions extends ProductTypePriceChangesActions {
     constructor(protected store: NgRedux<ProductTypePriceChangesAppState>) { super(store); }
+
+    public getMessageParams(record: LocalObject<ProductTypePriceChange, number>): StrIndex<string> {
+        return {
+            dates: `${record.object.validFrom} - ${record.object.validTo}`
+        };
+    }
 }
