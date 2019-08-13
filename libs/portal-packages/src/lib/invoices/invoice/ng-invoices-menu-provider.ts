1d8fbc15896fdf76b63d6378fe6fce995f2a86f1
Christian Vilen <christian-vilen@itinstituttet.dk>
2019-08-13 16:33:03 +0200

    Collapsed menu provider into one for invoices

--- libs/portal-packages/src/lib/invoices/invoice/ng-invoices-menu-provider.ts
+++ libs/portal-packages/src/lib/invoices/invoice/ng-invoices-menu-provider.ts
@@ -1,67 +1,114 @@
 import { Injectable } from '@angular/core';
 import { NgSkysmackStore } from '@skysmack/ng-skysmack';
-import { MenuArea, MenuProvider, SPEEDDIAL, SIDEBAR } from '@skysmack/framework';
+import { MenuArea, MenuProvider, SPEEDDIAL, SIDEBAR, Package, LocalObject } from '@skysmack/framework';
 import { MenuItem } from '@skysmack/framework';
 import { Guid } from 'guid-typescript';
 import { Observable } from 'rxjs';
-import { getMenuEntries, getCombinedMenuEntries } from '@skysmack/ng-framework';
+import { getMenuEntries, getCombinedMenuEntries, getConnectedPackageCustomMenuEntries, setBackButton } from '@skysmack/ng-framework';
 import { InvoicesPermissions } from '@skysmack/packages-invoices';
-import { InvoicesTypeId } from '@skysmack/package-types';
+import { InvoicesTypeId, InvoicesProductsTypeId } from '@skysmack/package-types';
 import { InvoicesIndexComponent } from './components/invoices-index/invoices-index.component';
+import { InvoiceItemsIndexComponent } from '../invoice-item/components/invoice-items-index/invoice-items-index.component';
+import { NgInvoiceItemsMenuProvider } from '../invoice-item/ng-invoice-items-menu-provider';
+import { InvoicesProductsAddProductsComponent } from '../../invoices-products/invoices-products/components/invoices-products-add-products/invoices-products-add-products.component';
+import { InvoicePaymentsIndexComponent } from '../invoice-payment/components/invoice-payments-index/invoice-payments-index.component';
 
 @Injectable({ providedIn: 'root' })
 export class NgInvoicesMenuProvider implements MenuProvider {
     public id = Guid.create().toString();
-    public translationPrefix = 'INVOICES.INDEX.';
+    public InvoicesTranslationPrefix = 'INVOICES.INDEX.';
+    public InvoiceItemsTranslationPrefix = 'INVOICE_ITEMS.INDEX.';
+    public InvoicePaymentsTranslationPrefix = 'INVOICE_PAYMENTS.INDEX.';
 
     constructor(
         public store: NgSkysmackStore
     ) { }
 
     public getMenuAreas(packagePath: string, componentKey: string): Observable<MenuArea[]> {
-        return getMenuEntries<MenuArea>(
-            packagePath,
-            InvoicesTypeId,
-            componentKey,
-            InvoicesIndexComponent.COMPONENT_KEY,
-            this.getInvoicesMenuAreas,
-            this.store
+        return getCombinedMenuEntries<MenuArea>(
+            getMenuEntries<MenuArea>(packagePath, InvoicesTypeId, componentKey, InvoicesIndexComponent.COMPONENT_KEY, this.getInvoicesMenuAreas, this.store),
+            getMenuEntries<MenuArea>(packagePath, InvoicesTypeId, componentKey, InvoiceItemsIndexComponent.COMPONENT_KEY, this.getInvoiceItemsMenuAreas, this.store),
+            getMenuEntries<MenuArea>(packagePath, InvoicesTypeId, componentKey, InvoicePaymentsIndexComponent.COMPONENT_KEY, this.getInvoicePaymentsMenuAreas, this.store)
         );
     };
 
 
 
     public getMenuItems(packagePath: string, componentKey: string): Observable<MenuItem[]> {
-        return getMenuEntries<MenuItem>(
-            packagePath,
-            InvoicesTypeId,
-            componentKey,
-            InvoicesIndexComponent.COMPONENT_KEY,
-            this.getInvoicesMenuItems,
-            this.store
-        )
+        return getCombinedMenuEntries<MenuItem>(
+            getMenuEntries<MenuItem>(packagePath, InvoicesTypeId, componentKey, InvoicesIndexComponent.COMPONENT_KEY, this.getInvoicesMenuItems, this.store),
+            getMenuEntries<MenuItem>(packagePath, InvoicesTypeId, componentKey, InvoiceItemsIndexComponent.COMPONENT_KEY, this.getInvoiceItemsMenuItems, this.store),
+            getConnectedPackageCustomMenuEntries(
+                packagePath,
+                InvoicesProductsTypeId,
+                InvoicesTypeId,
+                componentKey,
+                InvoiceItemsIndexComponent.COMPONENT_KEY,
+                this.store,
+                (_package: LocalObject<Package, string>) => new MenuItem({
+                    area: 'actions',
+                    providedIn: [SIDEBAR, SPEEDDIAL]
+                }).asEventAction(`${_package.object.name}`, (_this: NgInvoiceItemsMenuProvider) => {
+                    const dialogRef = _this.dialog.open(InvoicesProductsAddProductsComponent, {
+                        width: '500px',
+                        data: { packagePath: _package.object.path }
+                    });
+                }, 'monetization_on', this)
+            ),
+            getMenuEntries<MenuItem>(packagePath, InvoicesTypeId, componentKey, InvoicePaymentsIndexComponent.COMPONENT_KEY, this.getInvoicePaymentsMenuItems, this.store)
+        );
     };
 
-    public getInvoicesMenuAreas = () => {
+    private getInvoicesMenuAreas = () => {
         return [
             new MenuArea({
                 area: 'actions',
-                translationPrefix: this.translationPrefix,
+                translationPrefix: this.InvoicesTranslationPrefix,
                 order: 1
             }),
             new MenuArea({
                 area: 'manage',
-                translationPrefix: this.translationPrefix,
+                translationPrefix: this.InvoicesTranslationPrefix,
                 order: 2
             })
         ];
     };
 
-    public getInvoicesMenuItems = () => {
+    private getInvoiceItemsMenuAreas = () => {
+        return [
+            new MenuArea({
+                area: 'actions',
+                translationPrefix: this.InvoiceItemsTranslationPrefix,
+                order: 1
+            }),
+            new MenuArea({
+                area: 'manage',
+                translationPrefix: this.InvoiceItemsTranslationPrefix,
+                order: 2
+            })
+        ];
+    }
+
+    private getInvoicePaymentsMenuAreas = () => {
+        return [
+            new MenuArea({
+                area: 'actions',
+                translationPrefix: this.InvoicePaymentsTranslationPrefix,
+                order: 1
+            }),
+            new MenuArea({
+                area: 'manage',
+                translationPrefix: this.InvoicePaymentsTranslationPrefix,
+                order: 2
+            })
+        ];
+    };
+
+    private getInvoicesMenuItems = () => {
         return [
             new MenuItem({
                 url: 'create',
-                displayName: this.translationPrefix + 'CREATE',
+                displayName: this.InvoicesTranslationPrefix + 'CREATE',
                 area: 'actions',
                 order: 1,
                 icon: 'add',
@@ -72,7 +119,7 @@ export class NgInvoicesMenuProvider implements MenuProvider {
             }),
             new MenuItem({
                 url: 'fields',
-                displayName: this.translationPrefix + 'FIELDS',
+                displayName: this.InvoicesTranslationPrefix + 'FIELDS',
                 area: 'manage',
                 order: 2,
                 icon: 'short_text',
@@ -83,7 +130,7 @@ export class NgInvoicesMenuProvider implements MenuProvider {
             }),
             new MenuItem({
                 url: 'payments',
-                displayName: this.translationPrefix + 'PAYMENTS',
+                displayName: this.InvoicesTranslationPrefix + 'PAYMENTS',
                 area: 'manage',
                 order: 3,
                 icon: 'short_text',
@@ -92,6 +139,62 @@ export class NgInvoicesMenuProvider implements MenuProvider {
                 ],
                 providedIn: [SIDEBAR]
             })
+        ];
+    };
+
+    private getInvoiceItemsMenuItems = (packagePath: string): MenuItem[] => {
+        return [
+            new MenuItem({
+                url: 'create',
+                displayName: this.InvoiceItemsTranslationPrefix + 'CREATE',
+                area: 'actions',
+                order: 1,
+                icon: 'add',
+                permissions: [
+                    InvoicesPermissions.addInvoiceItems
+                ],
+                providedIn: [SIDEBAR, SPEEDDIAL]
+            }),
+            new MenuItem({
+                url: 'fields',
+                displayName: this.InvoiceItemsTranslationPrefix + 'FIELDS',
+                area: 'manage',
+                order: 2,
+                icon: 'short_text',
+                permissions: [
+                    InvoicesPermissions.findInvoiceItemFields
+                ],
+                providedIn: [SIDEBAR]
+            }),
+            setBackButton(packagePath)
+        ];
+    };
+
+    private getInvoicePaymentsMenuItems = (packagePath: string): MenuItem[] => {
+        return [
+            new MenuItem({
+                url: 'create',
+                displayName: this.InvoicePaymentsTranslationPrefix + 'CREATE',
+                area: 'actions',
+                order: 1,
+                icon: 'add',
+                permissions: [
+                    InvoicesPermissions.addInvoicePayments
+                ],
+                providedIn: [SIDEBAR, SPEEDDIAL]
+            }),
+            new MenuItem({
+                url: 'fields',
+                displayName: this.InvoicePaymentsTranslationPrefix + 'FIELDS',
+                area: 'manage',
+                order: 2,
+                icon: 'short_text',
+                permissions: [
+                    InvoicesPermissions.findInvoicePaymentFields
+                ],
+                providedIn: [SIDEBAR]
+            }),
+            setBackButton(packagePath)
         ];
     };
 }
