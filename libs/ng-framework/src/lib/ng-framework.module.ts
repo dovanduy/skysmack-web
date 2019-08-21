af86893ed0ec5586ee8288a3a72c10eabe1e334d
Martin Jul Hammer <mjh@itinstituttet.dk>
2019-08-21 11:54:12 +0200

    Fixed circular dependency

--- libs/ng-framework/src/lib/ng-framework.module.ts
+++ libs/ng-framework/src/lib/ng-framework.module.ts
@@ -1,5 +1,5 @@
 import { NgModule } from '@angular/core';
-import { directives } from '@skysmack/ng-framework';
+import { directives } from './directives/directives';
 
 @NgModule({
     imports: [],
