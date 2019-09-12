577412819db49723723617426e211cc91ad54cd1
Martin Jul Hammer <mjh@itinstituttet.dk>
2019-09-12 14:38:46 +0200

    File upload now works and can append a static name (currenltly same for all)

--- libs/portal-packages/src/lib/file-storage/file-storage/fields/multi-file-upload-field/multi-file-upload-field.component.ts
+++ libs/portal-packages/src/lib/file-storage/file-storage/fields/multi-file-upload-field/multi-file-upload-field.component.ts
@@ -1,7 +1,7 @@
-import { Component, OnInit, Inject } from '@angular/core';
+import { Component, OnInit, Inject, ViewChild } from '@angular/core';
 import { Field } from '@skysmack/ng-dynamic-forms';
 import { FieldBaseComponent } from '@skysmack/portal-fields';
-import { FileUploader } from 'ng2-file-upload';
+import { FileUploader, FileItem } from 'ng2-file-upload';
 import { Router } from '@angular/router';
 import { API_DOMAIN_INJECTOR_TOKEN, ApiDomain } from '@skysmack/framework';
 
@@ -26,8 +26,21 @@ export class MultiFileUploadFieldComponent extends FieldBaseComponent<Field> imp
     super.ngOnInit();
     const packagePath = this.router.url.split('/')[1];
     this.uploader = new FileUploader({ url: `${this.apiDomain.domain}/${packagePath}` });
+
+    this.uploader.onBuildItemForm = this.onBuildItemForm.bind(this);
   }
 
+  public upload(fileItem: FileItem) {
+    fileItem.alias = 'files';
+    console.log(fileItem)
+    fileItem.upload();
+    // console.log(this.uploader.queue);
+  }
+
+  private onBuildItemForm(fileItem: FileItem, form: FormData): any {
+    form.append('0.name', 'test/apple-01.png');
+    return { fileItem, form };
+  }
 
   public fileOverBase(e: any): void {
     this.hasBaseDropZoneOver = e;
