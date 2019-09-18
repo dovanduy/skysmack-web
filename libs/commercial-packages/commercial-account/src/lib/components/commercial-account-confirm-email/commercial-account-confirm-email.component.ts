487c428fcc98ed8c10ad69be3518dd1299a3101a
Martin Jul Hammer <mjh@itinstituttet.dk>
2019-09-18 14:22:32 +0200

    Commercial account confirm email is now finished.

--- libs/commercial-packages/commercial-account/src/lib/components/commercial-account-confirm-email/commercial-account-confirm-email.component.ts
+++ libs/commercial-packages/commercial-account/src/lib/components/commercial-account-confirm-email/commercial-account-confirm-email.component.ts
@@ -3,7 +3,8 @@ import { Field, FormHelper } from '@skysmack/ng-dynamic-forms';
 import { Observable, of } from 'rxjs';
 import { CommercialAccountConfirmEmailFieldsConfig } from './commercial-account-confirm-email-fields-config';
 import { Router, ActivatedRoute } from '@angular/router';
-import { map } from 'rxjs/operators';
+import { map, take } from 'rxjs/operators';
+import { CommercialAccountService } from '../../services/commercial-account.service';
 
 @Component({
   selector: 'ss-commercial-account-confirm-email',
@@ -13,11 +14,13 @@ import { map } from 'rxjs/operators';
 export class CommercialAccountConfirmEmailComponent implements OnInit {
 
   public fields$: Observable<Field[]>;
+  public message: string;
 
   constructor(
     public fieldsConfig: CommercialAccountConfirmEmailFieldsConfig,
     public router: Router,
-    private activatedRoute: ActivatedRoute
+    private activatedRoute: ActivatedRoute,
+    private service: CommercialAccountService
   ) { }
 
   ngOnInit() {
@@ -30,8 +33,19 @@ export class CommercialAccountConfirmEmailComponent implements OnInit {
 
   public onSubmit(fh: FormHelper) {
     fh.formValid(() => {
-      console.log(fh.form);
-      this.router.navigate(['account', 'dashboard']);
+      this.service.confirmEmail(fh.form.getRawValue()).pipe(
+        map(response => {
+          if (response.status >= 200 && response.status <= 299) {
+            this.message = 'Email confirmed. Redirecting to login...';
+            setTimeout(() => {
+              this.router.navigate(['/', 'account', 'login']);
+            }, 2000);
+          } else {
+            this.message = 'An error occurred. Please try again.'
+          }
+        }),
+        take(1)
+      ).subscribe();
     }, false);
   }
 }
