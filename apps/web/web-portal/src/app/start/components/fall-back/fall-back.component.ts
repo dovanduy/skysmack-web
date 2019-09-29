076b9e7e290c10b2d914af27a8427010ed020bc2
Morten Aslo-Petersen <morten@itinstituttet.dk>
2019-09-29 15:14:56 +0200

    Note on endless loop in fallback added

--- apps/web/web-portal/src/app/start/components/fall-back/fall-back.component.ts
+++ apps/web/web-portal/src/app/start/components/fall-back/fall-back.component.ts
@@ -2,7 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { Observable } from 'rxjs';
 import { Router, ActivatedRoute } from '@angular/router';
 import { PackageRouteConfiguration } from '@skysmack/portal-ui';
-import { take, filter, map } from 'rxjs/operators';
+import { filter, map } from 'rxjs/operators';
 import { NgSkysmackStore } from '@skysmack/ng-skysmack';
 
 @Component({
@@ -21,19 +21,26 @@ export class FallBackComponent implements OnInit {
   }
 
   ngOnInit() {
+    // TODO: This can cause and infinite loop. 
+    // If a package exists but an Angular module doesn't, it will redirect forever. 
+    // Solution could be either of these: 
+    // a) Check the package in registered modules, routes or similar, to ensure it exists in the Angular project and not only in the tenant.
+    // b) Set a static bool property, if last component was the FallBack component, and reset this as soon another component are visited. 
     this.packageNotFound$ = this.store.getSkysmack().pipe(
       filter(tenant => tenant && tenant.packages && tenant.packages.length > 0),
-      take(1),
-      map((tenant) => {         
+      map((tenant) => {
         const packagePath = this.router.url.split('/')[1].toLowerCase();
         if (tenant.packages.map(p => p.path.toLowerCase()).indexOf(packagePath) === -1) {
           // Package not found
           return true;
         }
         // Package was found, we shouldn't be here so let's do some magic redirect.
-        this.router.onSameUrlNavigation = 'reload';
-        this.router.navigateByUrl(this.router.url, { skipLocationChange: true });
-        this.router.onSameUrlNavigation = 'ignore';
+        // Timeout is to ensure routes are configured 
+        setTimeout(() => {
+          this.router.onSameUrlNavigation = 'reload';
+          this.router.navigateByUrl(this.router.url, { skipLocationChange: true });
+          this.router.onSameUrlNavigation = 'ignore';
+        }, 50);
         return false;
       }));
   }
