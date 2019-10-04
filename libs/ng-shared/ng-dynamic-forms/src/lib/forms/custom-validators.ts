8d7e28d06c16cad676372802c2676e4eb998fd19
Morten Aslo-Petersen <morten@itinstituttet.dk>
2019-10-04 16:59:47 +0200

    Bugfix for compare passwords

--- libs/ng-shared/ng-dynamic-forms/src/lib/forms/custom-validators.ts
+++ libs/ng-shared/ng-dynamic-forms/src/lib/forms/custom-validators.ts
@@ -20,16 +20,18 @@ export class CustomValidators {
             const passwordControl = controls[passwordFieldName];
             const confirmPasswordControl = controls[comparePasswordFieldName];
 
-            if (passwordControl && confirmPasswordControl) {
-                if (passwordControl.value !== confirmPasswordControl.value) {
+            if (passwordControl && confirmPasswordControl && ((passwordControl.value && passwordControl.value.length > 0) || (confirmPasswordControl.value && confirmPasswordControl.value.length > 0))) {
+                if (!passwordControl.value || !confirmPasswordControl.value || passwordControl.value !== confirmPasswordControl.value) {
                     const error = { passwordMismatch: true };
                     confirmPasswordControl.setErrors(error);
                     return error;
-                } else {
-                    confirmPasswordControl.setErrors(null);
-                    return null;
                 }
             }
+            
+            if (confirmPasswordControl) {
+                confirmPasswordControl.setErrors(null);
+            }
+            return null;
         };
     }
 
