b9d87f5573ee4febcab1077013c8d8e1e6c1123f
Martin Jul Hammer <mjh@itinstituttet.dk>
2019-10-07 15:33:41 +0200

    Fixed records getting related data added to their requests.

--- libs/packages/maintenance/src/models/assignment-type.ts
+++ libs/packages/maintenance/src/models/assignment-type.ts
@@ -1,8 +1,10 @@
-import { DocumentRecord } from "@skysmack/framework";
+import { DocumentRecord, LocalObject } from "@skysmack/framework";
+import { MaintenanceState } from './maintenance-state';
 
 export class AssignmentType extends DocumentRecord<number> {
     public description: string;
     public stateId: number;
+    public state: LocalObject<MaintenanceState, number>;
     public duePeriod: string;
 
     public constructor(init?: Partial<AssignmentType>) {
