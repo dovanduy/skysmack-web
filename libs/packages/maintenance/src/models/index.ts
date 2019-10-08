8c5fd9bc66adf95584e09147f8b12d9ad01935f7
Martin Jul Hammer <mjh@itinstituttet.dk>
2019-10-08 09:58:00 +0200

    Added scheduled assignment

--- libs/packages/maintenance/src/models/index.ts
+++ libs/packages/maintenance/src/models/index.ts
@@ -4,4 +4,4 @@ export * from './maintenance-state';
 export * from './assignments-schedule';
 export * from './assignment-type';
 export * from './maintenace-entity-status';
-
+export * from './scheduled-assignment';
