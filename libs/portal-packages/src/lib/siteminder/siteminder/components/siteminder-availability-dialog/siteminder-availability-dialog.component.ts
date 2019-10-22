b8039c9a29434b289e648a93ad77045744d3f1aa
Martin Jul Hammer <mjh@itinstituttet.dk>
2019-10-22 14:13:18 +0200

    Expanded siteminder availability dialog component

--- libs/portal-packages/src/lib/siteminder/siteminder/components/siteminder-availability-dialog/siteminder-availability-dialog.component.ts
+++ libs/portal-packages/src/lib/siteminder/siteminder/components/siteminder-availability-dialog/siteminder-availability-dialog.component.ts
@@ -1,4 +1,9 @@
-import { Component, OnInit, Input } from '@angular/core';
+import { Component, OnInit, Input, Inject } from '@angular/core';
+import { MAT_DIALOG_DATA, MatDialogRef } from '@angular/material/dialog';
+import { Availability } from 'libs/packages/siteminder/src';
+import { FormControl } from '@angular/forms';
+import { Observable, combineLatest } from 'rxjs';
+import { map, startWith } from 'rxjs/operators';
 
 @Component({
   selector: 'ss-siteminder-availability-dialog',
@@ -7,14 +12,37 @@ import { Component, OnInit, Input } from '@angular/core';
 })
 export class SiteMinderAvailabilityDialogComponent implements OnInit {
 
-  // @Input() public data: Availability;
-  public available: number;
-  public availableModifier: number;
-  public availableAfterModification: number;
+  public availableControl = new FormControl();
+  public availableModifierControl = new FormControl();
+  public availableAfterModification$: Observable<number>;
 
-  constructor() { }
+  constructor(
+    public dialogRef: MatDialogRef<SiteMinderAvailabilityDialogComponent>,
+    @Inject(MAT_DIALOG_DATA) public data: Availability
+  ) { }
 
   ngOnInit() {
+    const available = this.data.available;
+    const availableModifier = this.data.availableModifier
+
+    this.availableControl.setValue(available);
+    this.availableModifierControl.setValue(availableModifier);
+
+    this.availableAfterModification$ = combineLatest([
+      this.availableControl.valueChanges.pipe(startWith(available)),
+      this.availableModifierControl.valueChanges.pipe(startWith(availableModifier))
+    ]).pipe(
+      map(([available, availableModifier]) => available + availableModifier)
+    );
   }
 
+  public ok() {
+    // Update availability
+    console.log('Updating availability')
+    this.dialogRef.close();
+  }
+
+  public cancel() {
+    this.dialogRef.close();
+  }
 }
