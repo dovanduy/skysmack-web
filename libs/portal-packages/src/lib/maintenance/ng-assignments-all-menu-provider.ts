5983e7e917b6c407a8020851e197856e1a8b2d9a
Christian Vilen <christian-vilen@itinstituttet.dk>
2019-10-23 15:02:51 +0200

    Permissions fix

--- libs/portal-packages/src/lib/maintenance/ng-assignments-all-menu-provider.ts
+++ libs/portal-packages/src/lib/maintenance/ng-assignments-all-menu-provider.ts
@@ -171,7 +171,7 @@ export class NgAssignmentAllMenuProvider implements MenuProvider {
                 order: 1,
                 icon: 'add',
                 permissions: [
-                    MaintenancePermissions.addAssignments
+                    MaintenancePermissions.addSingleAssignments
                 ],
                 providedIn: [SIDEBAR, SPEEDDIAL]
             }),
