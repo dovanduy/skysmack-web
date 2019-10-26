e52d58b5f733435e5810be2d9fc129ed4adf4f93
Morten Aslo-Petersen <morten@itinstituttet.dk>
2019-10-26 11:56:18 +0200

    Date time format

--- libs/portal-ui/src/lib/pipes/display-value.pipe.ts
+++ libs/portal-ui/src/lib/pipes/display-value.pipe.ts
@@ -1,12 +1,33 @@
 import { Pipe, PipeTransform } from '@angular/core';
 import { DisplayColumn, getProperty } from '@skysmack/framework';
+import * as _moment from 'moment';
+const moment = _moment;
 
 @Pipe({ name: 'displayValue' })
 export class DisplayValuePipe implements PipeTransform {
     transform(entity: any, column: DisplayColumn) {
+        let myValue = entity;
         if (column.fieldDisplaySubKey) {
-            return getProperty(entity, column.fieldDisplaySubKey, false);
+            myValue = getProperty(entity, column.fieldDisplaySubKey, false);
         }
-        return entity;
+
+        var dateFormats = [
+            moment.ISO_8601,
+            "YYYY-MM-DD'T'HH:mm:ss"
+        ];
+
+        if (typeof myValue === 'string') {
+            if (/\/Date\((\d*)\)\//.exec(myValue)) {
+                myValue = new Date(myValue);
+            } else if (moment(myValue, dateFormats, true).isValid()) {
+                myValue = moment(myValue, dateFormats).toDate();
+            }
+        }
+
+        if (myValue instanceof Date) {
+            return moment(myValue).format('DD-MM-YYYY HH:mm');
+        }
+
+        return myValue;
     }
 }
