ce27d5e81bfc2761e228099e48835e55c5276088
Martin Jul Hammer <mjh@itinstituttet.dk>
2019-11-02 16:24:05 +0100

    LodgingTypeRatePlanChannel create now works.

--- libs/portal-packages/src/lib/siteminder/lodging-type-rate-plan-channels/components/siteminder-lodging-type-rate-plan-channels-create/siteminder-lodging-type-rate-plan-channels-create.component.ts
+++ libs/portal-packages/src/lib/siteminder/lodging-type-rate-plan-channels/components/siteminder-lodging-type-rate-plan-channels-create/siteminder-lodging-type-rate-plan-channels-create.component.ts
@@ -1,11 +1,13 @@
 import { Component, OnInit } from '@angular/core';
-import { LodgingTypeRatePlanChannel, SiteMinderLodgingTypeRatePlanChannelsAppState, LodgingTypeRatePlanChannelKey } from '@skysmack/packages-siteminder';
+import { LodgingTypeRatePlanChannel, SiteMinderLodgingTypeRatePlanChannelsAppState, LodgingTypeRatePlanChannelKey, LodgingTypeRatePlanKey } from '@skysmack/packages-siteminder';
 import { NgSiteMinderLodgingTypeRatePlanChannelsActions, NgSiteMinderLodgingTypeRatePlanChannelsStore } from '@skysmack/ng-siteminder';
 import { NgSkysmackStore } from '@skysmack/ng-skysmack';
 import { ActivatedRoute, Router } from '@angular/router';
 import { EditorNavService } from '@skysmack/portal-ui';
 import { RecordFormComponent } from '@skysmack/portal-fields';
 import { NgSiteMinderLodgingTypeRatePlanChannelsFieldsConfig } from '../../ng-siteminder-lodging-type-rate-plan-channels-fields-config';
+import { FormHelper } from '@skysmack/ng-dynamic-forms';
+import { toLocalObject, LocalObject } from '@skysmack/framework';
 
 @Component({
   selector: 'ss-siteminder-lodging-type-rate-plan-channels-create',
@@ -28,5 +30,21 @@ export class SiteMinderLodgingTypeRatePlanChannelsCreateComponent extends Record
   ngOnInit() {
     super.ngOnInit();
     this.setCreateFields();
+  }
+
+  protected create(fh: FormHelper) {
+    fh.formValid(() => {
+      const localObject = this.extractFormValues(fh) as unknown as LocalObject<{ channelId: number, lodgingTypeRatePlanId: LodgingTypeRatePlanKey }, LodgingTypeRatePlanChannelKey>;
+      const lodgingTypeRatePlanChannel = toLocalObject<LodgingTypeRatePlanChannel, LodgingTypeRatePlanChannelKey>(new LodgingTypeRatePlanChannel({
+        lodgingTypeId: localObject.object.lodgingTypeRatePlanId.lodgingTypeId,
+        ratePlanId: localObject.object.lodgingTypeRatePlanId.ratePlanId,
+        channelId: localObject.object.channelId
+      }));
+
+      this.editorItem ? localObject.localId = this.editorItem.localId : lodgingTypeRatePlanChannel.localId = lodgingTypeRatePlanChannel.localId;
+
+      this.actions.add([lodgingTypeRatePlanChannel], this.packagePath);
+      this.editorNavService.hideEditorNav();
+    });
   }
 }
