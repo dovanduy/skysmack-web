4e461c3cd2085e6ab3fa766b8541d1bb2a1cea27
Martin Jul Hammer <mjh@itinstituttet.dk>
2019-11-04 13:25:26 +0100

    Fixed build errors.

--- libs/packages/maintenance/src/redux/single-assignments/single-assignments-actions.ts
+++ libs/packages/maintenance/src/redux/single-assignments/single-assignments-actions.ts
@@ -1,13 +1,13 @@
 import { RecordActionsBase } from '@skysmack/redux';
+import { Injectable } from '@angular/core';
 import { SingleAssignmentsAppState } from './single-assignments-reducer';
 import { Store } from 'redux';
 import { SINGLE_ASSIGNMENTS_REDUX_KEY, SINGLE_ASSIGNMENTS_ADDITIONAL_PATHS } from '../../constants/constants';
 import { LocalObject, StrIndex } from '@skysmack/framework';
 import { SingleAssignment } from '../../models/single-assignment';
-import { Injectable } from '@angular/core';
 
-@Injectable({ providedIn: 'root' })
 export class SingleAssignmentsActions extends RecordActionsBase<SingleAssignmentsAppState, Store<SingleAssignmentsAppState>> {
+
     constructor(protected store: Store<SingleAssignmentsAppState>) { super(store, SINGLE_ASSIGNMENTS_REDUX_KEY, SINGLE_ASSIGNMENTS_ADDITIONAL_PATHS); }
 
     public getMessageParams(record: LocalObject<SingleAssignment, number>): StrIndex<string> {
