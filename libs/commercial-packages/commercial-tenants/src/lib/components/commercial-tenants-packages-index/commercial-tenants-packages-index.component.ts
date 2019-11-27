a500483003a9fe317e7783c6e00b31a92ec98148
Martin Jul Hammer <mjh@itinstituttet.dk>
2019-11-27 10:33:55 +0100

    Commercial: Restyled packages grid

--- libs/commercial-packages/commercial-tenants/src/lib/components/commercial-tenants-packages-index/commercial-tenants-packages-index.component.ts
+++ libs/commercial-packages/commercial-tenants/src/lib/components/commercial-tenants-packages-index/commercial-tenants-packages-index.component.ts
@@ -14,7 +14,6 @@ import { FormControl } from '@angular/forms';
 })
 export class CommercialTenantsPackagesIndexComponent implements OnInit {
   public availablePackages$: Observable<CommercialAvailablePackage[]>;
-  public noDepsAvailablePackages$: Observable<CommercialAvailablePackage[]>;
   public filteredAvailablePackages$: Observable<CommercialAvailablePackage[]>;
   public availablePackagesAutoCompleteControl = new FormControl();
 
@@ -26,10 +25,6 @@ export class CommercialTenantsPackagesIndexComponent implements OnInit {
   ngOnInit() {
     this.availablePackages$ = this.packagesService.getAvailablePackages().pipe(
       map((x: HttpSuccessResponse<CommercialAvailablePackage>) => x.body as CommercialAvailablePackage[]),
-    );
-
-    this.noDepsAvailablePackages$ = this.availablePackages$.pipe(
-      map(packages => packages.filter(_package => !_package.dependencyTypes || _package.dependencyTypes && _package.dependencyTypes.length === 0))
     );
 
     this.filteredAvailablePackages$ = combineLatest(
