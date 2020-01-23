f13bc867e918bd9a9af427ddf22d88d5810d2e77
Martin Jul Hammer <mjh@itinstituttet.dk>
2020-01-23 10:35:31 +0100

    OAuth2 packages is now included in the main section of the package drawer when users are not logged in.

--- libs/portal-ui/src/lib/components/common/package-drawer/package-drawer.component.ts
+++ libs/portal-ui/src/lib/components/common/package-drawer/package-drawer.component.ts
@@ -1,10 +1,11 @@
 import { Component, OnInit } from '@angular/core';
-import { Observable } from 'rxjs';
+import { Observable, combineLatest } from 'rxjs';
 import { NgSkysmackStore } from '@skysmack/ng-skysmack';
 import { Menu } from '@skysmack/framework';
 import { UIRedux } from './../../../redux/ui-redux';
-import { LoadedPackage } from '@skysmack/ng-framework';
-import { map } from 'rxjs/operators';
+import { LoadedPackage, NgAuthenticationStore } from '@skysmack/ng-framework';
+import { map, tap } from 'rxjs/operators';
+import { OAuth2TypeId } from '@skysmack/package-types';
 
 @Component({
   selector: 'ss-package-drawer',
@@ -19,14 +20,36 @@ export class PackageDrawerComponent implements OnInit {
 
   constructor(
     public skysmackStore: NgSkysmackStore,
+    public authStore: NgAuthenticationStore,
     public uiRedux: UIRedux
   ) { }
 
   ngOnInit() {
+    // Get loaded packages
     this.loadedPackages$ = this.skysmackStore.getLoadedPackages().pipe(map(loadedPackages => loadedPackages.filter(loadedPackage => loadedPackage._package.access)));
-    const hasManifest$ = this.loadedPackages$.pipe(map(packages => packages.filter(_package => !!_package.packageManifest)))
-    this.mainSectionPackages$ = hasManifest$.pipe(map(packages => packages.filter(_package => _package.packageManifest.menuLocation === 'main')));
+
+    // Ensure only those with manifest is used.
+    const hasManifest$ = this.loadedPackages$.pipe(map(packages => packages.filter(_package => !!_package.packageManifest)));
+
+    // A stream of oauth2 packages - will contain an empty array if the user is not logged in.
+    const oauth2Packages$ = combineLatest([
+      hasManifest$.pipe(map(loadedPackages => loadedPackages.filter(loadedPackage => loadedPackage._package.type === OAuth2TypeId))),
+      this.authStore.isCurrentUserAuthenticated()
+    ]).pipe(map(([oauth2Packages, isCurrentUserAuthenticated]) => isCurrentUserAuthenticated ? [] : oauth2Packages));
+
+    // Stream of packages belonging to the main section.
+    const includeInMainSectionPackages$ = hasManifest$.pipe(map(packages => packages.filter(_package => _package.packageManifest.menuLocation === 'main')));
+
+    // Packages to show in the main section of the package drawer.
+    this.mainSectionPackages$ = combineLatest([
+      oauth2Packages$,
+      includeInMainSectionPackages$
+    ]).pipe(map(([oauth2Packages, mainPackages]) => oauth2Packages.concat(mainPackages)));
+
+    // Packages to show in the docker section of the package drawer.
     this.dockerSectionPackages$ = hasManifest$.pipe(map(packages => packages.filter(_package => _package.packageManifest.menuLocation === 'docker')));
+
+    // The menu is needed to do checks if the package drawer is open/closed + some extras.
     this.menu$ = this.uiRedux.getMenu();
   }
 
