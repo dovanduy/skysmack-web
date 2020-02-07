aff6f6bb4b06522d25c0a2b33e0c96cc237c72c4
Martin Jul Hammer <mjh@itinstituttet.dk>
2020-02-07 15:01:08 +0100

    Drag and drop now work for key value fields component.

--- libs/portal-shared/portal-fields/src/lib/field-components/components/key-value-array-field/key-value-array-field.component.ts
+++ libs/portal-shared/portal-fields/src/lib/field-components/components/key-value-array-field/key-value-array-field.component.ts
@@ -2,10 +2,12 @@ import { Component, OnInit, ViewChild, ElementRef } from '@angular/core';
 import { FieldBaseComponent } from '../field-base-component';
 import { Field } from '@skysmack/ng-dynamic-forms';
 import { Guid } from 'guid-typescript';
+import { CdkDragDrop, moveItemInArray, transferArrayItem } from '@angular/cdk/drag-drop';
 
 @Component({
   selector: 'ss-key-value-array-field',
-  templateUrl: './key-value-array-field.component.html'
+  templateUrl: './key-value-array-field.component.html',
+  styleUrls: ['./key-value-array-field.component.scss']
 })
 export class KeyValueArrayFieldComponent extends FieldBaseComponent<Field> implements OnInit {
   @ViewChild('keyInput', { static: false }) public keyInput: ElementRef;
@@ -16,17 +18,17 @@ export class KeyValueArrayFieldComponent extends FieldBaseComponent<Field> imple
   ngOnInit() {
     super.ngOnInit();
     if (this.field.value) {
-    for (let index = 0; index < this.field.value.length; index++) {
-      const element = this.field.value[index];
-      
-      this.inputs.push({
-        id: Guid.create().toString(),
-        key: element.key,
-        value: element.value
-      });
+      for (let index = 0; index < this.field.value.length; index++) {
+        const element = this.field.value[index];
+
+        this.inputs.push({
+          id: Guid.create().toString(),
+          key: element.key,
+          value: element.value
+        });
+      }
+      this.setFieldValue(this.inputs.map(x => ({ key: x.key, value: x.value })));
     }
-    this.setFieldValue(this.inputs.map(x => ({ key: x.key, value: x.value })));
-  }
     // this.inputs = this.field.value ? this.field.value : [];
     // this.inputs.map(input => input.id = Guid.create().toString());
   }
@@ -51,5 +53,18 @@ export class KeyValueArrayFieldComponent extends FieldBaseComponent<Field> imple
     this.inputs = this.inputs.filter(input => input.id !== id);
     const values = this.inputs.map(x => ({ key: x.key, value: x.value }));
     this.setFieldValue(values.length > 0 ? values : null);
+  }
+
+  public drop(event: CdkDragDrop<string[]>) {
+    if (event.previousContainer === event.container) {
+      moveItemInArray(event.container.data, event.previousIndex, event.currentIndex);
+    } else {
+      transferArrayItem(event.previousContainer.data,
+        event.container.data,
+        event.previousIndex,
+        event.currentIndex);
+    }
+
+    this.setFieldValue(this.inputs);
   }
 }
